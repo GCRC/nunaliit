d38fcf6ddd0ac4f6c45b874cd937d0b2b6aa3cf1
Jean-Pierre Fiset <jp@fiset.ca>
2012-10-02 12:29:20 -0400

    nunaliit2-javascript: Insert eol at the end of each javascript file before minimizing.
    Issue #13

--- nunaliit2-javascript/src/main/java/ca/carleton/gcrc/javascript/CompressProcess.java
+++ nunaliit2-javascript/src/main/java/ca/carleton/gcrc/javascript/CompressProcess.java
@@ -1,8 +1,10 @@
 package ca.carleton.gcrc.javascript;
 
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.Reader;
@@ -52,13 +54,19 @@ public class CompressProcess {
 	public void generate(LibraryConfiguration config, Writer writer) throws Exception {
 		// Create input reader
 		Reader reader = null;
-		Vector<FileInputStream> streams = new Vector<FileInputStream>(); 
+		Vector<InputStream> streams = new Vector<InputStream>(); 
 		SequenceInputStream sis = null;
 		try {
+			// Create end of line byte array
+			byte[] eol = { (byte)'\n' };
+			
 			// Create sequence input stream from files in configuration
 			for(File file : config.getInputFiles()){
 				FileInputStream fis = new FileInputStream(file);
 				streams.add(fis);
+				
+				ByteArrayInputStream bais = new ByteArrayInputStream(eol);
+				streams.add(bais);
 			}
 			sis = new SequenceInputStream(streams.elements());
 			reader = new InputStreamReader(sis, "UTF-8");
@@ -77,7 +85,7 @@ public class CompressProcess {
 					// Ignore
 				}
 			}
-			for(FileInputStream fis : streams){
+			for(InputStream fis : streams){
 				try {
 					fis.close();
 				} catch (Exception e) {
