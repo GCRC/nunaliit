da2189bebdbd2d376679dc35f9e899254d76de0c
gbrauen <glenn@gbrauen.ca>
2012-10-23 12:15:46 -0400

    Adding nunaliit-utils and referencing from nunaliit-war-relations

---
+++ nunaliit2-utils/src/main/java/ca/carleton/gcrc/utils/ConfigUtils.java
@@ -0,0 +1,172 @@
+package ca.carleton.gcrc.utils;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.Properties;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+
+import org.apache.log4j.Logger;
+
+public class ConfigUtils {
+
+	final protected Logger logger = Logger.getLogger(this.getClass());
+
+	private File configurationDirectory = null;
+	private File fallbackConfigurationDirectory = null;
+	
+	public ConfigUtils() {
+		
+	}
+
+	public Properties loadProperties(String baseName, boolean loadDefault) throws ServletException {
+
+		// Load up contribution properties file....
+		Properties props = null;
+		{
+			File propFile = new File(configurationDirectory, baseName);
+			if( false == propFile.exists() || false == propFile.isFile() ) {
+				propFile = null;
+			}
+			if( null == propFile ) {
+				propFile = new File(fallbackConfigurationDirectory, baseName);
+				if( false == propFile.exists() || false == propFile.isFile() ) {
+					propFile = null;
+				}
+			}
+			if( loadDefault && null == propFile ) {
+				propFile = new File(fallbackConfigurationDirectory, baseName+".default");
+				if( false == propFile.exists() || false == propFile.isFile() ) {
+					propFile = null;
+				}
+			}
+			if( null == propFile ) {
+				logger.error("Property file location can not be determined for: "+baseName);
+			} else {
+				logger.info("Reading properties from "+propFile.getAbsolutePath());
+				FileInputStream fis = null;
+				try {
+					fis = new FileInputStream(propFile);
+					props = new Properties();
+					props.load(fis);
+				} catch (Exception e) {
+					logger.error("Unable to read properties from "+propFile.getAbsolutePath(),e);
+					props = null;
+				} finally {
+					if( null != fis ) {
+						try {
+							fis.close();
+						} catch (Exception e) {
+							// Ignore
+						}
+					}
+				}
+			}
+		}
+		
+		return props;
+	}
+	
+	public void computeConfigurationDirectories(ServletContext servletContext) throws ServletException {
+		
+		logger.info("Initializing Relations Configuration");
+
+		// Look for ./WEB-INF/atlas.properties
+		{
+			if( null != servletContext ) {
+				String realRootString = servletContext.getRealPath("./WEB-INF");
+				File realRoot = new File(realRootString);
+				if( realRoot.exists()
+				 && realRoot.isDirectory() ) {
+					File atlasPropsFile = new File(realRoot, "atlas.properties");
+					if( false == atlasPropsFile.exists() 
+					 || false == atlasPropsFile.isFile() ) {
+						atlasPropsFile = null;
+					}
+					if( null == atlasPropsFile ) {
+						atlasPropsFile = new File(realRoot, "atlas.properties.default");
+						if( false == atlasPropsFile.exists() 
+						 || false == atlasPropsFile.isFile() ) {
+							atlasPropsFile = null;
+						}
+					}
+					if( null != atlasPropsFile ) {
+						// Read in properties
+						Properties atlasProps = new Properties();
+						logger.info("Reading atlas properties from "+atlasPropsFile.getAbsolutePath());
+						FileInputStream fis = null;
+						try {
+							fis = new FileInputStream(atlasPropsFile);
+							atlasProps.load(fis);
+						} catch (Exception e) {
+							logger.error("Unable to read atlas properties from "+atlasPropsFile.getAbsolutePath(),e);
+						} finally {
+							if( null != fis ) {
+								try {
+									fis.close();
+								} catch (Exception e) {
+									// Ignore
+								}
+							}
+						}
+						
+						// Look for atlas.config.dir
+						if( atlasProps.containsKey("atlas.config.dir") ) {
+							String atlasConfigDirString = atlasProps.getProperty("atlas.config.dir");
+							logger.info("Atlas config directory specified: "+atlasConfigDirString);
+							File atlasConfigDir = new File(atlasConfigDirString);
+							if( atlasConfigDir.exists() && atlasConfigDir.isDirectory() ) {
+								// OK
+								configurationDirectory = atlasConfigDir;
+							} else {
+								logger.error("Invalid configuration directory specified. Ignoring.");
+							}
+						}
+						
+						// Look for atlas.name
+						if( atlasProps.containsKey("atlas.name") ) {
+							String atlasName = atlasProps.getProperty("atlas.name");
+							logger.info("Atlas name specified: "+atlasName);
+							File atlasConfigDir = new File("/etc/nunaliit2/atlas",atlasName);
+							if( atlasConfigDir.exists() && atlasConfigDir.isDirectory() ) {
+								// OK
+								configurationDirectory = atlasConfigDir;
+							} else {
+								logger.error("Configuration directory associated with name not found. Ignoring. "+atlasConfigDir.getAbsolutePath());
+							}
+						}
+					}
+				}
+			}
+		}
+		
+		// Check WEB-INF directory
+		{
+			if( null != servletContext ) {
+				String webInfString = servletContext.getRealPath("./WEB-INF");
+				File webInfDir = new File(webInfString);
+				if( webInfDir.exists() ) {
+					if( webInfDir.isDirectory() ) {
+						fallbackConfigurationDirectory = webInfDir;
+					}
+				}
+			}
+		}
+
+		if( null == configurationDirectory ) {
+			configurationDirectory = fallbackConfigurationDirectory;
+		}
+		
+		if( null == configurationDirectory ) {
+			throw new ServletException("Can not determine configuration directory");
+		}
+		if( null == fallbackConfigurationDirectory ) {
+			throw new ServletException("Can not determine fallback configuration directory");
+		}
+		
+		logger.info("Configuration directory: "+configurationDirectory.getAbsolutePath());
+		logger.info("Configuration directory on fallback: "+fallbackConfigurationDirectory.getAbsolutePath());
+	}
+
+}
--- nunaliit2-war-relations/src/main/java/ca/carleton/gcrc/relations/config/ConfigServlet.java
+++ nunaliit2-war-relations/src/main/java/ca/carleton/gcrc/relations/config/ConfigServlet.java
@@ -1,7 +1,5 @@
 package ca.carleton.gcrc.relations.config;
 
-import java.io.File;
-import java.io.FileInputStream;
 import java.sql.Connection;
 import java.util.Properties;
 
@@ -23,27 +21,25 @@ import ca.carleton.gcrc.search.SearchServlet;
 import ca.carleton.gcrc.upload.OnUploadedListenerSingleton;
 import ca.carleton.gcrc.upload.UploadServlet;
 import ca.carleton.gcrc.upload.UploadUtils;
+import ca.carleton.gcrc.utils.ConfigUtils;
 
 @SuppressWarnings("serial")
 public class ConfigServlet extends HttpServlet {
 
 	final protected Logger logger = Logger.getLogger(this.getClass());
 	
-	private File configurationDirectory = null;
-	private File fallbackConfigurationDirectory = null;
 	private JdbcConnections jdbcConnections = null;
+	private ConfigUtils configUtils = null;
 	
 	public ConfigServlet() {
-		
+		configUtils = new ConfigUtils();
 	}
 
 	public void init(ServletConfig config) throws ServletException {
 
-		logger.info("Initializing Relations Configuration");
-
 		// Figure out configuration directories
 		try {
-			computeConfigurationDirectories(config.getServletContext());
+			configUtils.computeConfigurationDirectories(config.getServletContext());
 		} catch(ServletException e) {
 			logger.error("Error while computing configuration directories",e);
 			throw e;
@@ -100,155 +96,8 @@ public class ConfigServlet extends HttpServlet {
 		logger.info("Completed Relations Configuration");
 	}
 	
-	private Properties loadProperties(String baseName, boolean loadDefault) throws ServletException {
-
-		// Load up contribution properties file....
-		Properties props = null;
-		{
-			File propFile = new File(configurationDirectory, baseName);
-			if( false == propFile.exists() || false == propFile.isFile() ) {
-				propFile = null;
-			}
-			if( null == propFile ) {
-				propFile = new File(fallbackConfigurationDirectory, baseName);
-				if( false == propFile.exists() || false == propFile.isFile() ) {
-					propFile = null;
-				}
-			}
-			if( loadDefault && null == propFile ) {
-				propFile = new File(fallbackConfigurationDirectory, baseName+".default");
-				if( false == propFile.exists() || false == propFile.isFile() ) {
-					propFile = null;
-				}
-			}
-			if( null == propFile ) {
-				logger.error("Property file location can not be determined for: "+baseName);
-			} else {
-				logger.info("Reading properties from "+propFile.getAbsolutePath());
-				FileInputStream fis = null;
-				try {
-					fis = new FileInputStream(propFile);
-					props = new Properties();
-					props.load(fis);
-				} catch (Exception e) {
-					logger.error("Unable to read properties from "+propFile.getAbsolutePath(),e);
-					props = null;
-				} finally {
-					if( null != fis ) {
-						try {
-							fis.close();
-						} catch (Exception e) {
-							// Ignore
-						}
-					}
-				}
-			}
-		}
-		
-		return props;
-	}
-	
-	private void computeConfigurationDirectories(ServletContext servletContext) throws ServletException {
-		
-		// Look for ./WEB-INF/atlas.properties
-		{
-			if( null != servletContext ) {
-				String realRootString = servletContext.getRealPath("./WEB-INF");
-				File realRoot = new File(realRootString);
-				if( realRoot.exists()
-				 && realRoot.isDirectory() ) {
-					File atlasPropsFile = new File(realRoot, "atlas.properties");
-					if( false == atlasPropsFile.exists() 
-					 || false == atlasPropsFile.isFile() ) {
-						atlasPropsFile = null;
-					}
-					if( null == atlasPropsFile ) {
-						atlasPropsFile = new File(realRoot, "atlas.properties.default");
-						if( false == atlasPropsFile.exists() 
-						 || false == atlasPropsFile.isFile() ) {
-							atlasPropsFile = null;
-						}
-					}
-					if( null != atlasPropsFile ) {
-						// Read in properties
-						Properties atlasProps = new Properties();
-						logger.info("Reading atlas properties from "+atlasPropsFile.getAbsolutePath());
-						FileInputStream fis = null;
-						try {
-							fis = new FileInputStream(atlasPropsFile);
-							atlasProps.load(fis);
-						} catch (Exception e) {
-							logger.error("Unable to read atlas properties from "+atlasPropsFile.getAbsolutePath(),e);
-						} finally {
-							if( null != fis ) {
-								try {
-									fis.close();
-								} catch (Exception e) {
-									// Ignore
-								}
-							}
-						}
-						
-						// Look for atlas.config.dir
-						if( atlasProps.containsKey("atlas.config.dir") ) {
-							String atlasConfigDirString = atlasProps.getProperty("atlas.config.dir");
-							logger.info("Atlas config directory specified: "+atlasConfigDirString);
-							File atlasConfigDir = new File(atlasConfigDirString);
-							if( atlasConfigDir.exists() && atlasConfigDir.isDirectory() ) {
-								// OK
-								configurationDirectory = atlasConfigDir;
-							} else {
-								logger.error("Invalid configuration directory specified. Ignoring.");
-							}
-						}
-						
-						// Look for atlas.name
-						if( atlasProps.containsKey("atlas.name") ) {
-							String atlasName = atlasProps.getProperty("atlas.name");
-							logger.info("Atlas name specified: "+atlasName);
-							File atlasConfigDir = new File("/etc/nunaliit2/atlas",atlasName);
-							if( atlasConfigDir.exists() && atlasConfigDir.isDirectory() ) {
-								// OK
-								configurationDirectory = atlasConfigDir;
-							} else {
-								logger.error("Configuration directory associated with name not found. Ignoring. "+atlasConfigDir.getAbsolutePath());
-							}
-						}
-					}
-				}
-			}
-		}
-		
-		// Check WEB-INF directory
-		{
-			if( null != servletContext ) {
-				String webInfString = servletContext.getRealPath("./WEB-INF");
-				File webInfDir = new File(webInfString);
-				if( webInfDir.exists() ) {
-					if( webInfDir.isDirectory() ) {
-						fallbackConfigurationDirectory = webInfDir;
-					}
-				}
-			}
-		}
-
-		if( null == configurationDirectory ) {
-			configurationDirectory = fallbackConfigurationDirectory;
-		}
-		
-		if( null == configurationDirectory ) {
-			throw new ServletException("Can not determine configuration directory");
-		}
-		if( null == fallbackConfigurationDirectory ) {
-			throw new ServletException("Can not determine fallback configuration directory");
-		}
-		
-		logger.info("Configuration directory: "+configurationDirectory.getAbsolutePath());
-		logger.info("Configuration directory on fallback: "+fallbackConfigurationDirectory.getAbsolutePath());
-	}
-
 	private void initJDBC(ServletContext context) throws ServletException {
-		Properties props = loadProperties("jdbc.properties", false);
+		Properties props = configUtils.loadProperties("jdbc.properties", false);
 		context.setAttribute(
 				JdbcConnections.PROPERTIES_SERVLET_ATTRIB_NAME
 				,props
@@ -262,7 +111,7 @@ public class ConfigServlet extends HttpServlet {
 	}
 
 	private void initMultimedia(ServletContext context) throws ServletException {
-		Properties props = loadProperties("multimedia.properties", true);
+		Properties props = configUtils.loadProperties("multimedia.properties", true);
 		
 		MultimediaConfiguration.configureFromProperties(props);
 	}
@@ -284,7 +133,7 @@ public class ConfigServlet extends HttpServlet {
 			throw new ServletException("Unable to configure onUpload process");
 		}
 		
-		Properties props = loadProperties("upload.properties", false);
+		Properties props = configUtils.loadProperties("upload.properties", false);
 		context.setAttribute(
 				UploadUtils.PROPERTIES_ATTRIBUTE
 				,props
@@ -292,7 +141,7 @@ public class ConfigServlet extends HttpServlet {
 	}
 
 	private void initContributions(ServletContext context) throws ServletException {
-		Properties props = loadProperties("contributions.properties", true);
+		Properties props = configUtils.loadProperties("contributions.properties", true);
 		context.setAttribute(
 				ContributionsUtils.PROPERTIES_SERVLET_ATTRIB_NAME
 				,props
@@ -300,7 +149,7 @@ public class ConfigServlet extends HttpServlet {
 	}
 
 	private void initSearch(ServletContext context) throws ServletException {
-		Properties props = loadProperties("search.properties", true);
+		Properties props = configUtils.loadProperties("search.properties", true);
 		context.setAttribute(
 				SearchServlet.PROPERTIES_SERVLET_ATTRIB_NAME
 				,props
