2c175a9b80ef81fe388a2f7ab47aa2b54d035be5
Jean-Pierre Fiset <jp@fiset.ca>
2013-03-28 16:19:44 -0400

    nunaliit2-js: Refactor $n2.couch.UserDb so that it is a subclass of Database. This is needed to easily access the design documents installed on the _users database.
    Issue #72

---
+++ nunaliit2-couch-user/src/main/resources/userDesignDocument/lists/text-lookahead.js
@@ -0,0 +1,49 @@
+function(head, req) {
+
+	provides("js",function(){
+
+		var top = req.query.top;
+		
+		var result = [];
+		var row;
+		while(row = getRow()) {
+			var word = row.key[1];
+			var count = row.value;
+			
+			result.push( [word,count] );
+		};
+		
+		result.sort(function(a,b){
+			// Sort by word count
+			if( a[1] < b[1] ) {
+				return 1;
+			};
+			if( a[1] > b[1] ) {
+				return -1;
+			};
+			// Sort lexicographically
+			if( a[0] < b[0] ) {
+				return -1;
+			};
+			if( a[0] > b[0] ) {
+				return 1;
+			};
+			return 0;
+		});
+		
+		var full = true;
+		if( top && top < result.length ) {
+			result = result.slice(0,top);
+			full = false;
+		};
+		
+		send('{"rows":');
+		
+		send( toJSON(result) );
+
+		// Returns whether the full listing was returned
+		send(',"all_rows":'+full);
+	
+		send('}');
+	});
+}
