d45c4871b08977794f3ac6f4a8499772a59c0c12
Jean-Pierre Fiset <jp@fiset.ca>
2013-04-12 08:40:22 -0400

    nunaliit2-couch-config: Add new rules relating to atlas administrators. Atlas administrators can add roles relating to their atlas.
    Issue #72

--- nunaliit2-couch-config/src/main/webapp/WEB-INF/userDesignAuth/validate_doc_update.js
+++ nunaliit2-couch-config/src/main/webapp/WEB-INF/userDesignAuth/validate_doc_update.js
@@ -1,13 +1,18 @@
 function(newDoc, oldDoc, userCtx) {
-	var isAdmin = (
-		(userCtx.roles.indexOf('_admin') !== -1)
-		|| (userCtx.roles.indexOf('administrator') !== -1)
-	);
+	
+	var reAtlasAdmin = /(.*)_administrator/;
+	var reAtlasVetter = /(.*)_vetter/;
+	var reAtlasLayer = /(.*)_layer_(.*)/;
+	var reGlobalLayer = /layer_(.*)/;
+	
+	var userInfo = getRoles(userCtx.roles);
+	
+	var isSystemAdmin = userInfo.admin;
 	
 	if (newDoc._deleted === true) {
 		// allow deletes by admins and matching users
 		// without checking the other fields
-		if( isAdmin || (userCtx.name == oldDoc.name) ) {
+		if( isSystemAdmin || (userCtx.name == oldDoc.name) ) {
 			return;
 		} else {
 			throw({forbidden: 'Only admins may delete other user docs.'});
@@ -50,27 +55,46 @@ function(newDoc, oldDoc, userCtx) {
 		});
 	}
 	
-	if( !isAdmin ) {
-		if (oldDoc) { // validate non-admin updates
-			if (userCtx.name !== newDoc.name) {
+	// Validate changes from a user (not a system administrator)
+	if( !isSystemAdmin ) {
+		// Verify role changes
+		var oldRoles = null;
+		if( oldDoc ){
+			oldRoles = oldDoc.roles;
+		};
+		var changes = getRoleChanges(oldRoles, newDoc.roles);
+
+		for(var i=0,e=changes.changed.length;i<e;++i){
+			var r = changes.changed[i];
+			
+			if( r.atlas ){
+				// This is a role for an atlas. Atlas admins are
+				// allowed to assign them
+				if( userInfo 
+				 && userInfo.atlas
+				 && userInfo.atlas[r.atlas]
+				 && userInfo.atlas[r.atlas].admin
+				){
+					// OK
+				} else {
+					throw({forbidden: 'Only atlas administrators may set atlas roles: '+r.atlas});
+				};
+				
+			} else {
+				// Not an atlas role. Only system admins can change those
+				throw({forbidden: 'Only administrators may set global roles'});
+			};
+		};
+		
+		// Only a user can modify his/her document, unless
+		// an atlas administrator
+		if (oldDoc) {
+			if( userInfo.atlasAdmin ) {
+				// OK, atlas admin is allowed
+			} else if (userCtx.name !== newDoc.name) {
 				throw({forbidden: 'You may only update your own user document.'});
 			}
-			// validate role updates
-			var oldRoles = oldDoc.roles.sort();
-			var newRoles = newDoc.roles.sort();
-			
-			if (oldRoles.length !== newRoles.length) {
-				throw({forbidden: 'Only _admin may edit roles'});
-			}
-			
-			for (var i = 0; i < oldRoles.length; i++) {
-				if (oldRoles[i] !== newRoles[i]) {
-					throw({forbidden: 'Only _admin may edit roles'});
-				}
-			}
-		} else if (newDoc.roles.length > 0) {
-			throw({forbidden: 'Only _admin may set roles'});
-		}
+		};
 	}
 	
 	// no system roles in users db
@@ -83,5 +107,148 @@ function(newDoc, oldDoc, userCtx) {
 	// no system names as names
 	if (newDoc.name[0] === '_') {
 		throw({forbidden: 'Username may not start with underscore.'});
+	}
+	
+	// Given two arrays of roles, figure out which ones are added and deleted. Return
+	// information about them
+	function getRoleChanges(prevRoles, curRoles){
+		var prevMap = {};
+		var curMap = {};
+		
+		if(prevRoles){
+			for(var i=0,e=prevRoles.length;i<e;++i){
+				prevMap[prevRoles[i]] = true;
+			};
+		};
+		
+		if(curRoles){
+			for(var i=0,e=curRoles.length;i<e;++i){
+				curMap[curRoles[i]] = true;
+			};
+		};
+		
+		var changes = {
+			added: []
+			,removed: []
+			,changed: []
+		};
+		
+		// Find previous roles that are no longer in role list
+		for(var r in prevMap){
+			if( !curMap[r] ){
+				var i = getRoleInfo(r);
+				changes.removed.push(i);
+				changes.changed.push(i);
+			};
+		};
+		
+		// Find current roles that were not in the role list
+		for(var r in curMap){
+			if( !prevMap[r] ){
+				var i = getRoleInfo(r);
+				changes.added.push(i);
+				changes.changed.push(i);
+			};
+		};
+		
+		return changes;
+	};
+
+	// Take an array of roles and accumulate information about them
+	function getRoles(roles){
+		var info = {
+			admin: false
+			,vetter: false
+			,layers: []
+			,atlasAdmin: false
+			,atlas:{
+				
+			}
+			,roles:[]
+		};
+		
+		for(var i=0,e=roles.length; i<e; ++i){
+			var r = roles[i];
+
+			var i = getRoleInfo(r);
+			
+			info.roles.push(i);
+			
+			if( i.atlas ){
+				var atlas = info.atlas[i.atlas];
+				if( !atlas ){
+					atlas = {
+						admin: false
+						,vetter: false
+						,layers: []
+					};
+					info.atlas[i.atlas] = atlas;
+				};
+				
+				if( i.admin ){
+					atlas.admin = true;
+					info.atlasAdmin = true;
+				};
+				if( i.vetter ){
+					atlas.vetter = true;
+				};
+				if( i.layer ){
+					atlas.layers.push(i.layer);
+				};
+				
+			} else {
+				// Global role
+				if( i.admin ){
+					info.admin = true;
+				};
+				if( i.vetter ){
+					info.vetter = true;
+				};
+				if( i.layer ){
+					info.layers.push(i.layer);
+				};
+			};
+		};
+		
+		return info;
+	};
+	
+	// Parse role string and return information about it
+	function getRoleInfo(r){
+		var role = {
+			name: r
+		};
+		
+		if( '_admin' === r 
+		 || 'administrator' === r ){
+			role.admin = true;
+			
+		} else if( 'vetter' == r ){
+			role.vetter = true;
+			
+		} else {
+			var mAtlasAdmin = reAtlasAdmin.exec(r);
+			var mAtlasVetter = reAtlasVetter.exec(r);
+			var mAtlasLayer = reAtlasLayer.exec(r);
+			var mGlobalLayer = reGlobalLayer.exec(r);
+
+			if( mAtlasAdmin ){
+				role.atlas = mAtlasAdmin[1];
+				role.admin = true;
+				
+			} else if( mAtlasVetter ){
+				role.atlas = mAtlasVetter[1];
+				role.vetter = true;
+				
+			} else if( mAtlasLayer ){
+				role.atlas = mAtlasLayer[1];
+				role.layer = mAtlasLayer[2];
+
+			} else if( mGlobalLayer ){
+				role.layer = mGlobalLayer[1];
+			};
+		};
+		
+		return role;
 	}
 }
