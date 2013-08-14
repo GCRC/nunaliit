e4bb2cc0e5f1dd16b379a83acc7ff4e1b2cb3bdb
Jean-Pierre Fiset <jp@fiset.ca>
2013-08-14 14:14:28 -0400

    nunaliit2-couch-application: In validate_doc_update function, do not use regular expression literals. Instead, use explicit RegExp constructor. This is to avoid bugs on versions of CouchDb where regexp literals are not supported.
    Issue #138

--- nunaliit2-couch-application/src/main/webapp/WEB-INF/atlas_couchapp/app/validate_doc_update.js
+++ nunaliit2-couch-application/src/main/webapp/WEB-INF/atlas_couchapp/app/validate_doc_update.js
@@ -3,12 +3,12 @@ function(newDoc, oldDoc, userCtxt) {
 	var n2utils = require('vendor/nunaliit2/n2.couchUtils');
 	var n2atlas = require('vendor/nunaliit2/atlas');
 	
-	var reAtlasAdmin = /(.*)_administrator/;
-	var reAtlasVetter = /(.*)_vetter/;
-	var reAtlasReplicator = /(.*)_replicator/;
-	var reAtlasUser = /(.*)_user/;
-	var reAtlasLayer = /(.*)_layer_(.*)/;
-	var reGlobalLayer = /layer_(.*)/;
+    var reAtlasAdmin = new RegExp("(.*)_administrator");
+    var reAtlasVetter = new RegExp("(.*)_vetter");
+    var reAtlasReplicator = new RegExp("(.*)_replicator");
+    var reAtlasUser = new RegExp("(.*)_user");
+    var reAtlasLayer = new RegExp("(.*)_layer_(.*)");
+    var reGlobalLayer = new RegExp("layer_(.*)");
 	
 	var publicLayerName = 'public';
 	var publicLayerPrefix = 'public_';
