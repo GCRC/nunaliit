5360003d44359e692b22de4f1072888b927ebfd2
Jean-Pierre Fiset <jp@fiset.ca>
2014-05-22 14:46:28 -0400

    nunaliit2-js: Add Array.indexOf() support to IE8

--- nunaliit2-js/src/main/webapp/nunaliit2/n2.jquery.js
+++ nunaliit2-js/src/main/webapp/nunaliit2/n2.jquery.js
@@ -45,6 +45,25 @@ if( browserInfo && browserInfo.browser === 'Explorer' ){
 					cache: false
 				});				
 			};
+			
+			// IE8 or older, Array.indexOf does not exist
+			if (!Array.prototype.indexOf) {
+				Array.prototype.indexOf = function(elt /*, from*/) {
+					var len = this.length >>> 0;
+					
+					var from = Number(arguments[1]) || 0;
+					from = (from < 0) ? Math.ceil(from) : Math.floor(from);
+					if (from < 0) from += len;
+					
+					for(; from < len; ++from) {
+						if( from in this 
+						 && this[from] === elt) {
+							return from;
+						};
+					};
+					return -1;
+				};
+			};
 		};
 	};
 };
