400ee42a3966ff205060e1b6127397cd96a91659
Jean-Pierre Fiset <jp@fiset.ca>
2015-09-18 20:30:40 -0400

    nunaliit2-js: Add lists
    Issue #375

--- nunaliit2-js/src/main/js/nunaliit2/n2.wiki.js
+++ nunaliit2-js/src/main/js/nunaliit2/n2.wiki.js
@@ -34,6 +34,196 @@ POSSIBILITY OF SUCH DAMAGE.
 //var _loc = function(str,args){ return $n2.loc(str,'nunaliit2',args); };
 
 //*******************************************************
+var regBlank = /^\s*$/;
+function isBlankLine(line){
+	if( '' === line ) return true;
+	if( regBlank.test(line) ){
+		return true;
+	};
+	return false;
+};
+
+//*******************************************************
+var regBlock = /^[=-]/;
+function isBlockLine(line){
+	if( isBlankLine(line) ) return true;
+	
+	if( regBlock.test(line) ){
+		return true;
+	};
+	return false;
+};
+
+//*******************************************************
+function getCharAt(line, index){
+	var c = undefined;
+	if( line.length > index ){
+		c = line[index];
+	};
+	return c;
+};
+
+//*******************************************************
+// Look at consecutive lines and merge them into one if
+// a line is a continuation of another
+function mergeLines(lines){
+	function isContinuingLine(line){
+		if( isBlockLine(line) ) return false;
+		if( '*' === line[0] ) return false;
+		if( '#' === line[0] ) return false;
+			
+		return true;
+	};
+	
+	var newLines = [];
+	
+	var previousLine = null;
+	
+	for(var i=0,e=lines.length; i<e; ++i){
+		var line = lines[i];
+	
+		if( null === previousLine ){
+			if( isBlockLine(line) ){
+				// This is a block line. Can not be continued
+				previousLine = null;
+			} else {
+				previousLine = line;
+			};
+
+			// Simply add this one
+			newLines.push(line);
+			
+			
+		} else {
+			if( isContinuingLine(line) ){
+				// Should continue previous line
+				previousLine = previousLine + ' ' + line;
+				newLines[newLines.length-1] = previousLine;
+				
+			} else {
+				
+				if( isBlockLine(line) ){
+					// This is a block line. Can not be continued
+					previousLine = null;
+				} else {
+					previousLine = line;
+				};
+
+				newLines.push(line);
+
+			};
+		};
+	};
+	
+	return newLines;
+};
+
+//*******************************************************
+function processLists(lines){
+	
+	function outputListLines(lines, offset, newLines){
+		var firstChar = getCharAt(lines[0],offset);
+		
+		if( '*' === firstChar ){
+			newLines.push('<ul>');
+		} else {
+			newLines.push('<ol>');
+		};
+
+		var isOpened = false;
+		
+		for(var i=0,e=lines.length; i<e; ++i){
+			var line = lines[i];
+
+			var c2 = getCharAt(line, offset+1);
+			
+			if( '*' === c2 || '#' === c2 ){
+				if( !isOpened ){
+					newLines.push('<li class="n2wiki">');
+				};
+
+				var listLines = [];
+				while( i<e ){
+					line = lines[i];
+					var c3 = getCharAt(line, offset+1);
+					if( c3 !== c2 ){
+						break;
+					} else {
+						listLines.push(line);
+						++i;
+					};
+				};
+				
+				if( i<e ){
+					--i;
+				};
+				
+				outputListLines(listLines, offset+1, newLines);
+				newLines.push('</li>');
+				isOpened = false;
+				
+			} else {
+				if( isOpened ){
+					newLines.push('</li><li class="n2wiki">');
+				} else {
+					newLines.push('<li class="n2wiki">');
+				};
+				newLines.push( line.substr(offset+1) );
+				isOpened = true;
+			};
+		};
+
+		if( isOpened ){
+			newLines.push('</li>');
+		};
+
+		if( '*' === firstChar ){
+			newLines.push('</ul>');
+		} else {
+			newLines.push('</ol>');
+		};
+	};
+	
+	var newLines = [];
+	
+	for(var i=0,e=lines.length; i<e; ++i){
+		var line = lines[i];
+		
+		var firstChar = getCharAt(line,0);
+
+		if( '*' === firstChar 
+		 || '#' === firstChar ){
+			
+			var listLines = [];
+			
+			while( i<e ){
+				line = lines[i];
+
+				var c1 = getCharAt(line, 0);
+				
+				if( c1 !== firstChar ){
+					break;
+				} else {
+					listLines.push( line );
+					++i;
+				};
+			};
+			
+			if( i<e ){
+				--i;
+			};
+			
+			outputListLines(listLines, 0, newLines);
+
+		} else {
+			newLines.push(line);
+		};
+	};
+	
+	return newLines;
+};
+
+//*******************************************************
 function WikiToHtml(opts_){
 	var opts = $n2.extend({
 		wiki: null
@@ -45,21 +235,36 @@ function WikiToHtml(opts_){
 	};
 
 	// Character escaping
-	text = text.replace(/&/g,'&amp;');
-	text = text.replace(/</g,'&lt;');
-	text = text.replace(/>/g,'&gt;');
-
-	// Headings
-	text = text.replace(/(?:^|\n)([=]+)(.*)\1/g, function (m, l, t) { // headings
-        return '<h' + l.length + ' class="n2wiki">' + t + '</h' + l.length + '>';
-    });
-
-	// Horizontal Line
-	text = text.replace(/(?:^|\n)([-]+)\s*/g, function (m, l, t) { // headings
-        return '<hr class="n2wiki"/>';
-    });
+	text = text.replace(/[&><\r]/g,function(m){
+		if( '&' === m ) return '&amp;';
+		if( '>' === m ) return '&gt;';
+		if( '<' === m ) return '&lt;';
+		if( '\r' === m ) return '';
+		return m;
+	});
 	
-	return text;
+	var lines = text.split('\n');
+	
+	lines = mergeLines(lines);
+	lines = processLists(lines);
+
+	for(var i=0,e=lines.length; i<e; ++i){
+		var line = lines[i];
+		
+		// Headings
+		line = line.replace(/(?:^)([=]+)(.*)\1/g, function (m, l, t) {
+	        return '<h' + l.length + ' class="n2wiki">' + t + '</h' + l.length + '>';
+	    });
+		
+		// Horizontal Line
+		line = line.replace(/(?:^|\n)([-]+)\s*/g, function (m) {
+	        return '<hr class="n2wiki"/>';
+	    });
+		
+		lines[i] = line;
+	};
+
+	return lines.join('');
 };
 
 //*******************************************************
