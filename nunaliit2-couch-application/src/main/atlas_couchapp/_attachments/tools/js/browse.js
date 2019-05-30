e2b8c60df4a2b7bd1abcae74ed62c89c82cf338b
boikle <boikle@users.noreply.github.com>
2019-05-30 16:19:25 -0400

    Upgraded browse tool to use the MDCTopAppBar and MDCDrawer.

--- nunaliit2-couch-application/src/main/atlas_couchapp/_attachments/tools/js/browse.js
+++ nunaliit2-couch-application/src/main/atlas_couchapp/_attachments/tools/js/browse.js
@@ -403,6 +403,31 @@ function main(opts_) {
 		}
 	});
 
+	//Tools Drawer
+	var drawer = new $n2.mdc.MDCDrawer({
+		hamburgerDrawer: true,
+		navHeaderTitle: 'Nunaliit Tools',
+		navItems: [
+			{"text": "User Management", "href": "./users.html"},
+			{"text": "Approval for Uploaded Files", "href": "./upload.html"},
+			{"text": "Data Browser", "href": "./browse.html", "activated": true},
+			{"text": "Localization", "href": "./translation.html"},
+			{"text": "Data Export", "href": "./export.html"},
+			{"text": "Data Modification", "href": "./select.html"},
+			{"text": "Schemas", "href": "./schemas.html"},
+			{"text": "Restore Tool", "href": "./restore.html"},
+			{"text": "Submission Tool", "href": "./submission.html"},
+			{"text": "Import Tool", "href": "./import.html"},
+			{"text": "Debug Tool", "href": "./debug.html"},
+			{"text": "Schema Editor", "href": "./schema_editor.html"}
+		]	
+	});
+
+	// Top-App-Bar
+	var topAppBar = new $n2.mdc.MDCTopAppBar({
+		barTitle: 'Data Browser'
+	});
+
 	config.start();
 };
 
