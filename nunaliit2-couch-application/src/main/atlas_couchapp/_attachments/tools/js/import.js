1fb7b9ea82314de8ef6dfefb28d36c6222181003
boikle <boikle@users.noreply.github.com>
2019-06-03 10:35:43 -0400

    Updated drawer and top app bar with MDC Objects on import tool page

--- nunaliit2-couch-application/src/main/atlas_couchapp/_attachments/tools/js/import.js
+++ nunaliit2-couch-application/src/main/atlas_couchapp/_attachments/tools/js/import.js
@@ -253,9 +253,36 @@
 
 		log( _loc('Upgrade Json Application Started') );
 	};
-
 	
+	function addHamburgerMenu(){
+		// Tools Drawer
+		var drawer = new $n2.mdc.MDCDrawer({
+			hamburgerDrawer: true,
+			navHeaderTitle: 'Nunaliit Tools',
+			navItems: [
+				{"text": "User Management", "href": "./users.html"},
+				{"text": "Approval for Uploaded Files", "href": "./upload.html"},
+				{"text": "Data Browser", "href": "./browse.html"},
+				{"text": "Localization", "href": "./translation.html"},
+				{"text": "Data Export", "href": "./export.html"},
+				{"text": "Data Modification", "href": "./select.html"},
+				{"text": "Schemas", "href": "./schemas.html"},
+				{"text": "Restore Tool", "href": "./restore.html"},
+				{"text": "Submission Tool", "href": "./submission.html"},
+				{"text": "Import Tool", "href": "./import.html", "activated": true},
+				{"text": "Debug Tool", "href": "./debug.html"},
+				{"text": "Schema Editor", "href": "./schema_editor.html"}
+			]	
+		});
+	
+		// Top-App-Bar
+		var topAppBar = new $n2.mdc.MDCTopAppBar({
+			barTitle: 'Import Data Tool'
+		});
+	};
+
 	$n2.importApp = {
 		main: main
+		,addHamburgerMenu: addHamburgerMenu
 	};
 })(jQuery,nunaliit2);
