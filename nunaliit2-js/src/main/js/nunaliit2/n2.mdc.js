71b1f3d47ca5d5157e4afe39343d1982e7ed61ab
Decheng Zhang <qrafzv88@gmail.com>
2019-10-30 12:16:34 -0400

    Merged in MDCCard and MDCSwitch Classes

--- nunaliit2-js/src/main/js/nunaliit2/n2.mdc.js
+++ nunaliit2-js/src/main/js/nunaliit2/n2.mdc.js
@@ -1738,6 +1738,113 @@ var MDCTopAppBar = $n2.Class('MDCTopAppBar', MDC, {
 	}
 });
 
+var MDCCard = $n2.Class('MDCCard', MDC, {
+	initialize: function(opts_){
+		var opts = $n2.extend({
+			parentElem: null,
+			mdcId: null,
+			mdcClasses: [],
+			mdcAttributes: null,
+			label: undefined,
+			imageGenerator: undefined,
+			infoGenerator: undefined
+		}, opts_);
+		this.onChangeCallBack = opts.onChangeCallBack;
+		this.infoGenerator = opts.infoGenerator;
+		this.imageGenerator = opts.imageGenerator;
+		this.label = opts.label;
+		this.initiallyOn = opts.initiallyOn;
+		MDC.prototype.initialize.call(this, opts);
+		this._generateMDCCard();
+	},
+	_generateMDCCard: function(){
+		
+		var _this = this;
+		var $card, card_info, media_thumb;
+		if ( this.infoGenerator ){
+			card_info = this.infoGenerator();
+		}
+		if ( this.imageGenerator ){
+			media_thumb = this.imageGenerator();
+		}
+		card_info = card_info ? card_info : '';
+		media_thumb = media_thumb ? media_thumb : '';
+		$card = $($.parseHTML('<div class="mdc-card">' // Outside container for mdc-card
+								+ '<div class="mdc-card__primary-action">'// For ripple effects
+									+ media_thumb// For left column thumbnail
+									+ '<div class="n2card__primary">'
+									+ card_info
+									+ '</div>'
+								+ '</div>'
+							+ '</div>'));
+		$card.appendTo(this.parentElem);
+		
+	}
+
+})
+var MDCSwitch = $n2.Class('MDCSwitch',MDC,{
+	initialize: function(opts_){
+		var opts = $n2.extend({
+			parentElem: null,
+			mdcId: null,
+			mdcClasses: [],
+			mdcAttributes: null,
+			onChangeCallBack: undefined,
+			label: undefined,
+			initiallyOn : false
+		}, opts_);
+		this.onChangeCallBack = opts.onChangeCallBack;
+		this.label = opts.label;
+		this.initiallyOn = opts.initiallyOn;
+		MDC.prototype.initialize.call(this, opts);
+		this._generateMDCSwitch();
+	},
+	_generateMDCSwitch: function(){
+		var _this = this;
+		var $switch;
+		var label_t = 'off/on';
+		if (this.label){
+			label_t = this.label;
+		}
+		if (this.initiallyOn){
+			$switch = $($.parseHTML('<div class="mdc-switch mdc-switch--checked"><div class="mdc-switch__track">'+
+					'</div><div class="mdc-switch__thumb-underlay">'+
+						'<div class="mdc-switch__thumb">'+
+								'<input type="checkbox" id="basic-switch" class="mdc-switch__native-control" role="switch" checked>'+
+							'</div>'+
+						'</div>'+
+				'</div>'+
+				'<label for="basic-switch">' + 
+				label_t +
+				'</label>'));
+		} else {
+			$switch = $($.parseHTML('<div class="mdc-switch"><div class="mdc-switch__track">'+
+					'</div><div class="mdc-switch__thumb-underlay">'+
+						'<div class="mdc-switch__thumb">'+
+								'<input type="checkbox" id="basic-switch" class="mdc-switch__native-control" role="switch">'+
+							'</div>'+
+						'</div>'+
+				'</div>'+
+				'<label for="basic-switch">off/on</label>'));
+		}
+		$switch.appendTo(this.parentElem);
+		
+		//add evt listener for 'change' in mdc way
+		var vanilla = new mdc.switchControl.MDCSwitch(document.querySelector('.mdc-switch'));
+		if (this.onChangeCallBack 
+				&& typeof this.onChangeCallBack === 'function'){
+			vanilla.nativeControl_.addEventListener('change', function(){
+				_this.onChangeCallBack(this.checked);
+			})
+		}
+		
+		if (showService) {
+			showService.fixElementAndChildren($('#' + this.mdcId));
+		}
+	}
+});
+
+
 $n2.mdc = {
 	Service: Service,
 	MDC: MDC,
@@ -1755,7 +1862,9 @@ $n2.mdc = {
 	MDCTabBar: MDCTabBar,
 	MDCTagBox: MDCTagBox, 
 	MDCTextField: MDCTextField,
-	MDCTopAppBar: MDCTopAppBar
+	MDCTopAppBar: MDCTopAppBar,
+	MDCSwitch: MDCSwitch,
+	MDCCard: MDCCard
 };
 
 })(jQuery,nunaliit2);
