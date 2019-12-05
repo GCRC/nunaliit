4d123fbc92ea02dc181473efb4228db23c99c04f
boikle <boikle@users.noreply.github.com>
2019-12-05 09:01:20 -0500

    Updated select component on import tool page.

--- nunaliit2-couch-application/src/main/atlas_couchapp/_attachments/tools/js/import.js
+++ nunaliit2-couch-application/src/main/atlas_couchapp/_attachments/tools/js/import.js
@@ -43,7 +43,7 @@
 				var importProfile = importProfiles[i];
 				importProfileMenuOpts.push({
 					"value": importProfile.getId(),
-					"label": _loc(importProfile.getLabel()) + ' (' + importProfile.getType() + ')'
+					"text": _loc(importProfile.getLabel()) + ' (' + importProfile.getType() + ')'
 				});
 			}
 		} else {
@@ -53,6 +53,7 @@
 		// Import Profile Select Menu
 		var importProfileSelect = new $n2.mdc.MDCSelect({
 			parentElem: $buttonLine,
+			mdcClasses: ['import_profile'],
 			menuLabel: 'Import Profile',
 			menuOpts: importProfileMenuOpts
 		});
@@ -76,7 +77,7 @@
 			btnLabel: 'Verify',
 			onBtnClick: function(){
 				var $inputSection = $appDiv.find('.importAppInput');
-				var importProfileId = $inputSection.find('.import_profile').val();
+				var importProfileId = importProfileSelect.getSelectedValue();
 				var importData = $inputSection.find('.importData').val();
 				
 				var error = false;
