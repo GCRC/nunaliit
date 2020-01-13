b907362a50e3d0009d4f1804908f3d6c8f099547
sruest <sruest@gcrc.carleton.ca>
2020-01-13 13:38:16 -0500

    fix #819 1) Minor update to Jetty version (9.2->9.4). 2) Update servlet-api version to keep in line with Jetty version. 3) Update other Java libraries to latest versions.

--- nunaliit2-couch-command/src/main/java/ca/carleton/gcrc/couch/command/impl/TransparentProxyFixedEscaped.java
+++ nunaliit2-couch-command/src/main/java/ca/carleton/gcrc/couch/command/impl/TransparentProxyFixedEscaped.java
@@ -1,81 +1,51 @@
 package ca.carleton.gcrc.couch.command.impl;
 
-import java.net.URI;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-import javax.servlet.http.HttpServletRequest;
-
 import org.eclipse.jetty.proxy.ProxyServlet;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 @SuppressWarnings("serial")
 public class TransparentProxyFixedEscaped extends ProxyServlet.Transparent {
-	
-	static final private Pattern rDesign2f = Pattern.compile("/_design%2[fF]");
 
-	final protected Logger logger = LoggerFactory.getLogger(this.getClass());
-    
-    static public String unescapeUriString(String uriString){
+	private static final Pattern rDesign2f = Pattern.compile("/_design%2[fF]");
+
+	protected final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	public static String unescapeUriString(String uriString){
 		Matcher mDesign2f = rDesign2f.matcher(uriString);
 		if( mDesign2f.find() ){
-			String fixedUriString = 
-					uriString.substring(0, mDesign2f.start())
+			return uriString.substring(0, mDesign2f.start())
     				+ "/_design/"
     				+ uriString.substring(mDesign2f.end());
-			
-			return fixedUriString;
 		}
 		
 		return null;
     }
 
-    @Override
-    protected URI rewriteURI(HttpServletRequest request)
-    {
-    	URI uri = super.rewriteURI(request);
+	@Override
+	protected String rewriteTarget(HttpServletRequest request)
+	{
+		String target = super.rewriteTarget(request);
 
-    	if( null != uri ){
-    		String uriStr = uri.toString();
-    		String fixedUriString = unescapeUriString(uriStr);
-    		if( null != fixedUriString ){
-    			try {
-					URI fixedUri = new URI(fixedUriString);
-					
-					logger.debug("proxy decode "+uri+" -> "+fixedUri);
-					
-					uri = fixedUri;
-					
-				} catch (Exception e) {
-					logger.error("Unable to decode URI: "+uri, e);
+		if (null != target) {
+			String fixedTarget = unescapeUriString(target);
+			if (null != fixedTarget) {
+				try {
+					logger.debug(String.format("proxy decode %s -> %s", target, fixedTarget));
+
+					target = fixedTarget;
+
 				}
-    		}
-    	}
-    	
-    	return uri;
-    }
+				catch (Exception e) {
+					logger.error("Unable to decode URI: " + target, e);
+				}
+			}
+		}
 
-    // This is the code for a later version of Jetty
-//    protected String rewriteTarget(HttpServletRequest request)
-//    {
-//    	String target = super.rewriteTarget(request);
-//
-//    	if( null != target ){
-//    		String fixedTarget = unescapeUriString(target);
-//    		if( null != fixedTarget ){
-//    			try {
-//					logger.debug("proxy decode "+target+" -> "+fixedTarget);
-//					
-//					target = fixedTarget;
-//					
-//				} catch (Exception e) {
-//					logger.error("Unable to decode URI: "+target, e);
-//				}
-//    		}
-//    	}
-//    	
-//    	return target;
-//    }
-    
+		return target;
+    }
 }
