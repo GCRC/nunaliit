d8082272ecaa171b31956f2f3d376a7936cbf38c
Amos Hayes <ahayes@gcrc.carleton.ca>
2020-01-31 16:51:26 -0500

    Undo public layer auto-approve in servlet
    Need to discuss workflow.

--- nunaliit2-couch-submission/src/main/java/ca/carleton/gcrc/couch/submission/impl/SubmissionRobotThread.java
+++ nunaliit2-couch-submission/src/main/java/ca/carleton/gcrc/couch/submission/impl/SubmissionRobotThread.java
@@ -287,10 +287,10 @@ public class SubmissionRobotThread extends Thread implements CouchDbChangeListen
 					String layerId = nunaliitLayers.optString(i);
 					if( null != layerId ){
 						if( "public".equals(layerId) ){
-							atLeastOneLayer = true;
+							//atLeastOneLayer = true;
 							// Public layer, ignore
 						} else if( layerId.startsWith("public_") ){
-							atLeastOneLayer = true;
+							//atLeastOneLayer = true;
 							// Public layer, ignore
 						} else {
 							atLeastOneLayer = true;
