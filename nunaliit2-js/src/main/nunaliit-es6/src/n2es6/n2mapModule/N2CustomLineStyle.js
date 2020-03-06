947cd8b4341cc93935c94e64d68730da6be313b7
Decheng Zhang <qrafzv88@gmail.com>
2020-03-06 10:07:55 -0500

    Mv nunaliit-es6 folder into nunaliit-js; Cleanup

---
+++ nunaliit2-js/src/main/nunaliit-es6/src/n2es6/n2mapModule/N2CustomLineStyle.js
@@ -0,0 +1,23 @@
+/**
+* @module n2es6/n2mapModule/N2CustomLineStyle
+*/
+import RegularShape from 'ol/style/RegularShape'
+import Fill from 'ol/style/Fill'
+import Stroke from 'ol/style/Stroke'
+/**
+ * @classdesc
+ * Custom style for feature linestring.
+ * @api
+ */
+class N2CustomLineStyle extends RegularShape{
+	constructor(opt_options) {
+		var options = $n2.extend({
+			
+		}, opt_options);
+		
+	}
+	render_(){
+		
+	}
+}
+export default N2CustomLineStyle
