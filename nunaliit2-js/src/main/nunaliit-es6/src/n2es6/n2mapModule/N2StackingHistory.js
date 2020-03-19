dc67476f9128ec07df3639feac8fb17ebce0ab22
decheng-on-dell <qrafzv88@gmail.com>
2020-03-19 13:43:02 -0400

    n2es6 src folder cleanup and keep align with branch-for-atlascine as much as possible

--- nunaliit2-js/src/main/nunaliit-es6/src/n2es6/n2mapModule/N2StackingHistory.js
+++ nunaliit2-js/src/main/nunaliit-es6/src/n2es6/n2mapModule/N2StackingHistory.js
@@ -4,7 +4,7 @@
 import {listen, unlisten} from 'ol/events.js';
 import hash from 'object-hash';
 class N2StackingHistory {
-	
+
 	constructor(opt_options){
 		var opts = $n2.extend({
 			map: null,
@@ -13,14 +13,14 @@ class N2StackingHistory {
 		this._history = {};
 		this._map = opts.map;
 		this._precision = opts.precision;
-		listen(this._map, 'postrender', this.clear , this);
+		listen(this._map, 'postcompose', this.clear , this);
 		//listen(this._map, '', , this);
 	}
 	getStackingHistory(ext){
 		var rst = undefined;
 		var hashkey = undefined;
 		if (Array.isArray(ext) && ext.length === 4){
-			hashkey = hash(ext)
+			hashkey = ext[0] + '' + ext[1];
 		}
 		if ( this._history[hashkey] ){
 			rst = this._history[hashkey] ;
@@ -30,7 +30,7 @@ class N2StackingHistory {
 	addStackingHistory(ext, extraInfo){
 		var hashkey = undefined;
 		if (Array.isArray(ext) && ext.length === 4){
-			hashkey = hash(ext);
+			hashkey =  ext[0] +''+ ext[1];
 		};
 		var pointInfo = {
 			extra: extraInfo
@@ -40,7 +40,7 @@ class N2StackingHistory {
 		}
 		this._history[hashkey].push(pointInfo);
 		return this._history[hashkey];
-		
+
 	}
 	_isCloseEnough(f1, f2){
 		var precision = this._precision;
@@ -55,16 +55,19 @@ class N2StackingHistory {
 			|| ext2[0] !== ext2[2]){
 			return false;
 		}
-		if ( Math.abs(ext1[0] - ext2[0]) <= precision 
+		if ( Math.abs(ext1[0] - ext2[0]) <= precision
 			&& Math.abs(ext1[1] - ext2[1]) <= precision ){
 			rst = true;
 		}
 		return rst;
 	}
 	clear(){
-		this._history = {};
+		for (var key in this._history) {
+    	delete this._history[key];
+		}
+
 	}
-	
+
 }
 
 export default N2StackingHistory;
