6eb1ba247e2408051cecc586b5ecec73a1e0c62c
decheng-on-dell <qrafzv88@gmail.com>
2020-03-19 13:23:35 -0400

    Revert the submissionRobotThread authentication logic to keep it aligned with 2.3.0s logic

--- nunaliit2-couch-submission/src/main/java/ca/carleton/gcrc/couch/submission/impl/SubmissionRobotThread.java
+++ nunaliit2-couch-submission/src/main/java/ca/carleton/gcrc/couch/submission/impl/SubmissionRobotThread.java
@@ -242,7 +242,6 @@ public class SubmissionRobotThread extends Thread implements CouchDbChangeListen
 		// Find roles associated with the user who submitted the change
 		List<String> roles = new Vector<String>();
 		JSONObject submissionInfo = submissionDoc.getJSONObject("nunaliit_submission");
-		String submitter_name = submissionInfo.optString("submitter_name");
 		JSONArray jsonRoles = submissionInfo.optJSONArray("submitter_roles");
 		if( null != jsonRoles ){
 			for(int i=0,e=jsonRoles.length(); i<e; ++i){
@@ -253,10 +252,8 @@ public class SubmissionRobotThread extends Thread implements CouchDbChangeListen
 		
 		JSONObject submittedDoc = submissionInfo.optJSONObject("submitted_doc");
 		JSONArray nunaliitLayers = null;
-		JSONObject nunaliitCreated = null;
 		if( null != submittedDoc ){
 			nunaliitLayers = submittedDoc.optJSONArray("nunaliit_layers");
-			nunaliitCreated = submittedDoc.optJSONObject("nunaliit_created");
 		}
 
 		// Check if submission should be automatically approved
@@ -290,9 +287,10 @@ public class SubmissionRobotThread extends Thread implements CouchDbChangeListen
 					String layerId = nunaliitLayers.optString(i);
 					if( null != layerId ){
 						if( "public".equals(layerId) ){
+							//atLeastOneLayer = true;
 							// Public layer, ignore
 						} else if( layerId.startsWith("public_") ){
-							atLeastOneLayer = true;
+							//atLeastOneLayer = true;
 							// Public layer, ignore
 						} else {
 							atLeastOneLayer = true;
@@ -314,18 +312,6 @@ public class SubmissionRobotThread extends Thread implements CouchDbChangeListen
 			};
 		}
 
-		// Check if document is owned by submitter
-		if ( !approved ) {
-			if ( null != submitter_name ) {
-				if( null != nunaliitCreated ){
-					String creator = nunaliitCreated.getString("name");
-					if ( submitter_name.equals(creator)) {
-						approved = true;
-					}
-				}
-			}
-		}
-		
 		if( approved ) {
 			CouchDb submissionDb = submissionDbDesignDocument.getDatabase();
 			submissionDoc.getJSONObject("nunaliit_submission")
