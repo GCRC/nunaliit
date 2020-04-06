bba6687f18bd261ce2026b87a840f5094a0103dc
boikle <boikle@users.noreply.github.com>
2020-04-06 13:17:06 -0400

    Added licence text to file head.

--- nunaliit2-js/src/main/js/nunaliit2/n2.widgetDateRange.js
+++ nunaliit2-js/src/main/js/nunaliit2/n2.widgetDateRange.js
@@ -1,17 +1,36 @@
 /*
- * Name: n2.widgetDateRange.js
- * ----------------------------------------------------------------------------
- * Description:
- * ----------------------------------------------------------------------------
- */
+Copyright (c) 2020, Geomatics and Cartographic Research Centre, Carleton
+University
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+ - Redistributions of source code must retain the above copyright notice,
+   this list of conditions and the following disclaimer.
+ - Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+ - Neither the name of the Geomatics and Cartographic Research Centre,
+   Carleton University nor the names of its contributors may be used to
+   endorse or promote products derived from this software without specific
+   prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+*/
 
 ;(function($,$n2) {
 	"use strict";
-
-	// Localization
-	var _loc = function(str,args) {
-		return $n2.loc(str,'nunaliit2-couch',args);
-	};
 
 	// Define Dispatcher Handle
 	var DH = 'n2.widgetDateRange';
@@ -33,7 +52,7 @@
 
 		endDate: null,
 
-		initialize: function(opts_){
+		initialize: function(opts_) {
 			var opts = $n2.extend({
 				containerId: null
 				,dispatchService: null
@@ -64,8 +83,7 @@
 		},
 
 		_display: function() {
-			var $container, $startDate, $endDate, $btn;
-			var $widgetWindow, $widgetWindowStart, $widgetWindowEnd;
+			var $container, $widgetWindow, $widgetWindowStart, $widgetWindowEnd;
 			var _this = this;
 			var containerId = this.containerId;
 
@@ -76,7 +94,7 @@
 				.addClass('n2widget_date_range')
 				.appendTo($('#' + containerId));
 
-			$btn = $('<div>')
+			$('<div>')
 				.addClass('n2widget_date_range_button')
 				.text("-- / --")
 				.appendTo($container)
@@ -97,7 +115,7 @@
 				.addClass('n2widget_date_range_end')
 				.appendTo($widgetWindow);
 
-			$startDate = $('<input>')
+			$('<input>')
 				.attr('id', 'start_date')
 				.attr('name', 'start_date')
 				.attr('type', 'text')
@@ -119,7 +137,7 @@
 				}
 			}).appendTo($widgetWindowStart);
 
-			$endDate = $('<input>')
+			$('<input>')
 				.attr('id', 'end_date')
 				.attr('name', 'end_date')
 				.attr('type', 'text')
@@ -140,24 +158,21 @@
 					_this._dateRangeUpdated();
 				}
 			}).appendTo($widgetWindowEnd);
-
-
 		},
 
 		_dateRangeUpdated: function() {
 			var _this = this;
-
 			var $startDate = $('#start_date');
 			var $endDate = $('#end_date');
 			var startDateVal = $startDate.val();
 			var endDateVal = $endDate.val();
 
+			// Set the end date to null if end date is less than the start date
 			if (startDateVal
 				&& endDateVal
 				&& startDateVal >= endDateVal) {
 				$endDate.text('');
 				$endDate.val(null);
-
 				_this.endDate = null;
 			}
 
@@ -182,18 +197,19 @@
 
 	// -------------------------------------------------------------------------
 	function HandleWidgetDisplayRequests(m) {
+		var widgetOptions, containerId, config, options, key, value, i;
+		var optionKeys = Object.keys(widgetOptions);
+
 		if (m.widgetType === 'dateRangeWidget') {
-			var widgetOptions = m.widgetOptions;
-			var containerId = m.containerId;
-			var config = m.config;
+			widgetOptions = m.widgetOptions;
+			containerId = m.containerId;
+			config = m.config;
+			options = {};
 
-			var options = {};
-
-			if (widgetOptions) {
-				for (var key in widgetOptions) {
-					var value = widgetOptions[key];
-					options[key] = value;
-				}
+			for (i = 0; i < optionKeys.length; i += 1) {
+				key = optionKeys[i];
+				value = widgetOptions[key];
+				options[key] = value;
 			}
 
 			options.containerId = containerId;
