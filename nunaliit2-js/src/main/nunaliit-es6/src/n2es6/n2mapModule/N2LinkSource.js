c0b608ded4ff397407d743961e99f150eeb1a179
alexgao1 <alexgao1@users.noreply.github.com>
2022-01-18 16:45:02 -0500

    Parse coordinates and visibility flags, add styler function, give lines width/strength and visibility

--- nunaliit2-js/src/main/nunaliit-es6/src/n2es6/n2mapModule/N2LinkSource.js
+++ nunaliit2-js/src/main/nunaliit-es6/src/n2es6/n2mapModule/N2LinkSource.js
@@ -4,9 +4,9 @@
 
 import VectorSource from 'ol/source/Vector';
 import LineString from 'ol/geom/LineString';
+import Stroke from 'ol/style/Stroke';
+import Style from 'ol/style/Style';
 import Feature from 'ol/Feature.js';
-/* import {listen, unlisten, unlistenByKey} from 'ol/events.js';
-import EventType from 'ol/events/EventType.js'; */
 
 /**
  * @classdesc
@@ -17,13 +17,6 @@ class N2LinkSource extends VectorSource {
 	constructor(options) {
 		super(options);
 		this._dispatchService = options.dispatchService;
-		/* this._source = options.source || null;
-		this._sourceChangeKey = null; */
-		/* if (options.source){
-			this._source = options.source;
-			this._sourceChangeKey =
-				listen(this.source, EventType.CHANGE, this.refresh, this); */
-
 		this._pointFeatures = [];
 		this._linestringFeatures = [];
 		this._timeCoordinateData = new Map();
@@ -49,6 +42,7 @@ class N2LinkSource extends VectorSource {
 			const timeB = featureB.data._ldata.start;
 			return ((timeA > timeB) ? 1 : (timeA < timeB) ? -1 : 0);
 		});
+
 		this._collectTimeCoordinateData();
 		this._generateLinkStrengths();
 		this._generateLineStringFeatures();
@@ -57,50 +51,62 @@ class N2LinkSource extends VectorSource {
 
 	_collectTimeCoordinateData() {
 		this._pointFeatures.forEach(point => {
-			const featureDuration = point.data._ldata.start;
-			
-			/* const featureGeomBbox = point.data.nunaliit_geom.bbox;
-			// clusters don't have bbox
-			const pointCoordinates = [featureGeomBbox[0], featureGeomBbox[1]]; */
+			const featureStartTime = point.data._ldata.start;
+			const isFeatureCurrentlyShowing = point.get("isVisible");
+			const pointCoordinates = [...point.getGeometry().flatCoordinates];
 
-			const pointCoordinates = [point.n2ConvertedBbox[0], point.n2ConvertedBbox[1]];
-			if (this._timeCoordinateData.has(featureDuration)) {
-				const pointLocations = this._timeCoordinateData.get(featureDuration);
-				pointLocations.push(pointCoordinates);
+			if (this._timeCoordinateData.has(featureStartTime)) {
+				let pointData = this._timeCoordinateData.get(featureStartTime);
+				pointData.push({
+					coordinates: pointCoordinates,
+					isFeatureVisible: isFeatureCurrentlyShowing,
+					i: point.data._ldata.timeLinkTags.placeTag
+				});
 			}
 			else {
-				this._timeCoordinateData.set(featureDuration, [pointCoordinates]);
+				this._timeCoordinateData.set(featureStartTime, [{ 
+					coordinates: pointCoordinates,
+					isFeatureVisible: isFeatureCurrentlyShowing,
+					i: point.data._ldata.timeLinkTags.placeTag
+				}]);
 			}
 		});
 	}
 
 	_generateLinkStrengths() {
-		let previousPoints = [];
-		this._timeCoordinateData.forEach((currPoints, _) => {
-			for (let i = 0; i < previousPoints.length; i++) {
-				for (let j = 0; j < currPoints.length; j++) {
-					/* cluster check for strength here */
-					const prevPointString = previousPoints[i].toString();
-					const currPointString = currPoints[j].toString();
+		let previousData = [];
+		this._timeCoordinateData.forEach((currentData, _) => {
+			for (let i = 0; i < previousData.length; i++) {
+				for (let j = 0; j < currentData.length; j++) {
+					const previousDataPoint = previousData[i];
+					const currentDataPoint = currentData[j];
 
-					if (prevPointString === currPointString) continue;
+					const prevPointString = previousDataPoint.coordinates.toString();
+					const currPointString = currentDataPoint.coordinates.toString();
+
+					if (prevPointString === currPointString) continue; // Not drawing a link from a point to the same point
 					let linkStrengthKey = `${prevPointString} ${currPointString}`;
 					if (prevPointString > currPointString) {
 						linkStrengthKey = `${currPointString} ${prevPointString}`;
 					}
+
+					const lineStringVisibility = (previousDataPoint.isFeatureVisible && currentDataPoint.isFeatureVisible);
+
 					if (this._linkStrengths.has(linkStrengthKey)) {
 						this._linkStrengths.get(linkStrengthKey).strength += 1;
+						this._linkStrengths.get(linkStrengthKey).isLinkVisible = lineStringVisibility;
 					}
 					else {
 						this._linkStrengths.set(linkStrengthKey, {
-							start: previousPoints[i],
-							end: currPoints[j],
+							start: previousDataPoint.coordinates,
+							end: currentDataPoint.coordinates,
+							isLinkVisible: lineStringVisibility,
 							strength: 1
 						});
 					}
 				}
 			}
-			previousPoints = currPoints;
+			previousData = currentData;
 		});
 	}
 
@@ -110,7 +116,19 @@ class N2LinkSource extends VectorSource {
 				geometry: new LineString([lineData.start, lineData.end])
 			});
 			lineStringFeature.setId(key);
+			lineStringFeature.set("isVisible", lineData.isLinkVisible, false);
+			lineStringFeature.set("linkStrength", lineData.strength, false);
 			this._linestringFeatures.push(lineStringFeature);
+		});
+	}
+
+	stylerFunction(feature) {
+		if (feature.get("isVisible") === false) return;
+		return new Style({
+			stroke: new Stroke({
+				width: feature.get("linkStrength"),
+				color: "blue"
+			})
 		});
 	}
 }
