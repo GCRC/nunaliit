ec0487d7c384ca546f884958628fbd04d4b80e5f
Brendan Billingsley <billingb@gmail.com>
2022-03-30 11:46:39 -0600

    Refine bulk upload. Add status and error reporting.

--- nunaliit2-couch-application/src/main/atlas_couchapp/_attachments/tools/js/bulkUpload.js
+++ nunaliit2-couch-application/src/main/atlas_couchapp/_attachments/tools/js/bulkUpload.js
@@ -1,203 +1,19 @@
 ; (function ($, $n2) {
 
-	var DH = 'browse.js';
+	var DH = 'bulkUpload.js';
 
-	var HASH_SEARCH_PREFIX = "search_";
-	var HASH_EDIT_PREFIX = "edit_";
 	var HASH_NEW_PREFIX = "new_";
 
-	var atlasDb = null;
-	var atlasDesign = null;
 	var uploadService = null;
-	var couchEditor = null;
 	var schemaEditorService = null;
-	var defaultSchema = null;
-	var searchServer = null;
-	var searchInput = null;
-	var requests = null;
-	var contributions = null;
-	var showService = null;
 	var schemaRepository = null;
 	var dispatcher = null;
 	var authService = null;
-	var createDocProcess = null;
 	var dialogService = null;
 	let filesToUpload = null;
 	let selectedSchema = null;
 	let documentSource = null;
 	let templateDoc = null;
-
-	function reportErrorsOnElem(errors, $elem) {
-		$elem.append($('<div>Error occurred during the request<div>'));
-
-		for (var i = 0, e = errors.length; i < e; ++i) {
-			var e = errors[i];
-			if (typeof (e) === 'object') {
-				e = JSON.stringify(e);
-			};
-			if (typeof (e) === 'string') {
-				$elem.append($('<div>' + e + '<div>'));
-			};
-		};
-	};
-
-	function reportError() {
-		$('.olkit_wait').remove();
-
-		$('#schemaValues').empty();
-		reportErrorsOnElem(arguments, $('#schemaValues'));
-	};
-
-	function startRequestWait() {
-		$('#schemaValues').html('<div class="olkit_wait"></div>');
-	};
-
-	function initiateEdit(docId) {
-		// Get document
-		startRequestWait();
-		atlasDb.getDocument({
-			docId: docId
-			, onSuccess: function (doc) {
-				var schemaName = doc.nunaliit_schema;
-				if (!schemaName) {
-					showEdit(doc);
-				} else {
-					schemaRepository.getSchema({
-						name: schemaName
-						, onSuccess: function (schema) {
-							showEdit(doc, schema);
-						}
-						, onError: function () {
-							showEdit(doc);
-						}
-					});
-				};
-			}
-			, onError: reportError
-		});
-
-		function showEdit(doc, schema) {
-			$('#schemaValues').empty();
-
-			couchEditor.cancelDocumentForm({ suppressEvents: true });
-
-			// Couch Editor
-			var couchEditorId = $n2.getUniqueId();
-			var $couchEditorDiv = $('<div id="' + couchEditorId + '"></div>');
-			$('#schemaValues').append($couchEditorDiv);
-			couchEditor.showDocumentForm(doc, {
-				panelName: couchEditorId
-				, schema: schema
-				, onCloseFn: function (doc_, editor, closeOptions) {
-					if (closeOptions.inserted) {
-						initiateEdit(doc._id);
-					} else if (closeOptions.updated) {
-						initiateEdit(doc._id);
-					};
-				}
-			});
-
-			// References to other objects
-			$('#schemaValues').append($('<h3>Other documents referenced by this document</h3>'));
-			var references = [];
-			$n2.couchUtils.extractLinks(doc, references);
-			var $table = $('<table></table>');
-			$('#schemaValues').append($table);
-			for (var i = 0, e = references.length; i < e; ++i) {
-				var docId = references[i].doc;
-				var $tr = $('<tr></tr>');
-
-				$table.append($tr);
-
-				var $td = $('<td class="docId"></td>');
-				$tr.append($td);
-
-				var $a = $('<a href="#' + docId + '" alt="' + docId + '">' + docId + '</a>');
-				showService.printBriefDescription($a, docId);
-				$td.append($a);
-				$a.click(function () {
-					var $a = $(this);
-					var docId = $a.attr('alt');
-					initiateEdit(docId);
-					return true;
-				});
-
-				var $td = $('<td class="brief"></td>');
-				$tr.append($td);
-			};
-
-			// References from other objects
-			$('#schemaValues').append($('<h3>Other documents referencing this document</h3>'));
-			$('#schemaValues').append($('<div id="referencesToThis"><div class="olkit_wait"></div></div>'));
-			atlasDesign.queryView({
-				viewName: 'link-references'
-				, startkey: doc._id
-				, endkey: doc._id
-				, onSuccess: function (rows) {
-					var $table = $('<table></table>');
-					$('#referencesToThis').empty().append($table);
-					for (var i = 0, e = rows.length; i < e; ++i) {
-						var docId = rows[i].id;
-						var $tr = $('<tr></tr>');
-
-						$table.append($tr);
-
-						var $td = $('<td class="docId"></td>');
-						$tr.append($td);
-
-						var $a = $('<a href="#' + docId + '" alt="' + docId + '">' + docId + '</a>');
-						$td.append($a);
-						$a.click(function () {
-							var $a = $(this);
-							var docId = $a.attr('alt');
-							initiateEdit(docId);
-							return true;
-						});
-						showService.printBriefDescription($a, docId);
-					};
-				}
-				, onError: reportError
-			});
-
-			// Attachments
-			if (doc._attachments) {
-				$('#schemaValues').append($('<h3>Attachments</h3>'));
-				var $table = $('<table></table>');
-				$('#schemaValues').append($table);
-				for (var key in doc._attachments) {
-					var att = doc._attachments[key];
-					var $tr = $('<tr></tr>');
-
-					$table.append($tr);
-
-					var $td = $('<td class="attachment"></td>');
-					$tr.append($td);
-
-					var attUrl = atlasDb.getDocumentUrl(doc);
-					attUrl = attUrl + '/' + key;
-					var $a = $('<a href="' + attUrl + '">' + key + '</a>');
-					$td.append($a);
-
-					var type = '';
-					if (att.content_type) {
-						type = att.content_type;
-					};
-					var $td = $('<td class="type">' + type + '</td>');
-					$tr.append($td);
-
-					var size = '';
-					if (att.length) {
-						size = att.length;
-					};
-					var $td = $('<td class="type">' + size + '</td>');
-					$tr.append($td);
-				};
-			};
-
-			var $errors = $('<div id="editErrors"></div>');
-			$('#schemaValues').append($errors);
-		};
-	};
 
 	function addDocument() {
 		if (authService && !authService.isLoggedIn()) {
@@ -224,14 +40,13 @@
 
 	function createNewDocumentFromSchemaName(schemaName) {
 		if (schemaName) {
-			$n2.log('schemaName', schemaName);
 			schemaRepository.getSchema({
 				name: schemaName
 				, onSuccess: function (schema) {
 					createNewDocument(schema);
 				}
 				, onError: function (err) {
-					reportError('Unable to get selected schema: ' + err);
+					$('#uploadErrors').append(`<p>Unable to get selected schema: ${err}</p>`);
 				}
 			});
 		} else {
@@ -240,7 +55,6 @@
 	};
 
 	function createNewDocument() {
-
 		if (selectedSchema) {
 			templateDoc = selectedSchema.createObject({});
 		} else {
@@ -250,66 +64,22 @@
 		$n2.couchDocument.adjustDocument(templateDoc);
 
 		showEdit(templateDoc, selectedSchema);
+	}
 
-		function showEdit(doc, schema) {
-			$('#schemaValues').empty();
+	function showEdit(doc, schema) {
+		$('#schemaValues').empty();
+		$('#uploadForm').removeClass('hiddenForm');
 
-			this.schemaEditor = schemaEditorService.editDocument({
-				doc: doc
-				, schema: schema
-				, $div: $('#schemaValues')
-				, onChanged: function () {
-					// _this._adjustInternalValues(_this.editedDocument);
-					// if( _this.treeEditor ) {
-					// 	_this.treeEditor.refresh();
-					// };
-					// if( _this.slideEditor ) {
-					// 	_this.slideEditor.refresh();
-					// };
-					// if( _this.attachmentEditor ) {
-					// 	_this.attachmentEditor.refresh();
-					// };
-					// _this._refreshRelations(data);
-					// _this.onEditorObjectChanged(data);
-					console.log(doc);
-				}
-			});
-			// couchEditor.cancelDocumentForm({suppressEvents:true});
-
-
-
-			// couchEditor.showDocumentForm(doc,{
-			// 	panelName: 'results'
-			// 	,schema: schema
-			// 	,onFeatureInsertedFn: function(fid,feature){
-			// 	}
-			// 	,onCancelFn: function(){ 
-			// 	}
-			// });
-			$n2.log('schema', schema);
-			// $n2.log('couchEditor',couchEditor);
-
-			var $errors = $('<div id="editErrors"></div>');
-			$('#schemaValues').append($errors);
-		};
-	};
-
-	function setNewHash(newHash) {
-		if (null === newHash) {
-			location.hash = null;
-		} else {
-			location.hash = '#' + encodeURIComponent(newHash);
-		};
-	};
+		this.schemaEditor = schemaEditorService.editDocument({
+			doc: doc
+			, schema: schema
+			, $div: $('#schemaValues')
+			, onChanged: function () {}
+		});
+	}
 
 	function _handle(m) {
-		if ('selected' === m.type) {
-			initiateEdit(m.docId);
-
-		} else if ('unselected' === m.type) {
-			$('#schemaValues').empty();
-
-		} else if ('hashChanged' === m.type) {
+		if ('hashChanged' === m.type) {
 			var hash = m.hash;
 			if (hash.substr(0, HASH_NEW_PREFIX.length) === HASH_NEW_PREFIX) {
 				// New document
@@ -318,23 +88,52 @@
 				createNewDocumentFromSchemaName(schemaName);
 			};
 		};
-	};
+	}
 
 	function getUuid() {
 		return new Promise(function (resolve, reject) {
 			documentSource.getUniqueIdentifier({
-				onSuccess: function(uuid){	
+				onSuccess: function (uuid) {
 					resolve(uuid);
 				},
-				onError: function(err) {
+				onError: function (err) {
 					reject();
 				}
-		  	});
+			});
 		});
 	}
 
-	//function checkUploadService()
-	//function createDocument()
+	function checkUploadService() {
+		return new Promise(function (resolve, reject) {
+			if (uploadService) {
+				uploadService.getWelcome({
+					onSuccess: function () {
+						return resolve(true);
+					},
+					onError: function () {
+						return resolve(false);
+					}
+				});
+			} else {
+				return resolve(false);
+			}
+		})
+	}
+
+	function createDocument(newDoc) {
+		return new Promise(function (resolve, reject) {
+			documentSource.createDocument({
+				doc: newDoc
+				, onSuccess: function (createdDoc) {
+					resolve(createdDoc);
+				}
+				, onError: function (err) {
+					reject(err);
+				}
+			})
+		});
+	}
+
 	function uploadMediaFile(file, i, uploadFileId) {
 		let data = new FormData();
 		data.append('media', file);
@@ -344,73 +143,91 @@
 		return new Promise(function (resolve, reject) {
 			$.ajax({
 				type: 'POST'
-				,url: `${uploadService.options.url}put`
-				,data: data
-				,dataType: 'json'
-				,processData: false
-				,contentType: false
-				,success: function(res) {
-					if( res.error ) {
+				, url: `${uploadService.options.url}put`
+				, data: data
+				, dataType: 'json'
+				, processData: false
+				, contentType: false
+				, success: function (res) {
+					if (res.error) {
 						reject(res.error);
 					} else {
 						resolve(res);
 					}
 				}
-				,error: function(xhr, status, err) {
+				, error: function (xhr, status, err) {
 					reject(err);
 				}
 			});
 		})
 	}
-		  
-	function uploadFiles(event) {
-		console.log("Handler for .submit() called.");
-		console.log(event);
-		if (uploadService) {
-			// Verify that server is available
-			uploadService.getWelcome({
-				onSuccess: async function () {
-					for (let i = 0; i < filesToUpload.length; i++) {
-						const uploadFile = filesToUpload[i];
-						const uploadFileId = await getUuid();
-						
-						let newDoc = selectedSchema.createObject({});
-						newDoc[templateDoc.nunaliit_schema] = templateDoc[templateDoc.nunaliit_schema];
 
-						// Compute a new attachment name
-						var attName = 'media';
+	function refreshStateTable(createdDocs) {
+		const resultsDiv = $('#results');
+		resultsDiv.html('');
+		const table = $('<table>');
+		const tbody = $('<tbody>');
 
-						// Update document
-						newDoc.nunaliit_attachments.files[attName] = {
-							attachmentName: attName
-							,status: "waiting for upload"
-							,data: {}
-							,uploadId: uploadFileId
-						};
+		createdDocs.forEach(docInfo => {
+			$(`<tr><td>${docInfo.id}</td><td>${docInfo.uploadId}</td><td>${docInfo.filename}</td><td>${docInfo.state}</td></tr>`).appendTo(tbody);
+		})
+		$('<thead><tr><th>ID</th><th>Media Upload ID</th><th>File Name</th><th>Status</th></tr></thead>').appendTo(table);
+		tbody.appendTo(table);
+		table.appendTo(resultsDiv);
+	}
 
-						documentSource.createDocument({
-							doc: newDoc
-							,onSuccess: async function(createdDoc) {
-								console.log('DOC CREATED with');
-								console.log(createdDoc);
-								console.log(`uploadId: ${uploadFileId}`);
-								console.log('DOC CREATED END');
-								await uploadMediaFile(uploadFile, i, uploadFileId);
-								console.log(`${createdDoc._id}, ${uploadFileId}, ${uploadFile.name}`);
-							}
-							,onError: function(err) {
-								console.log('Error creating doc');
-							}
-						})
-					}
-				}
-				, onError: function (err) {
-					_this._enableControls();
-					$n2.reportErrorForced('Server is not available: ' + err);
-				}
-			});
+	async function uploadFiles() {
+		$('#uploadErrors').text('');
+		let createdDocs = [];
+		const uploadAvailable = await checkUploadService();
+		if (!uploadAvailable) {
+			$('#uploadErrors').text('Upload service not available.');
+			return
 		}
-	}	
+		if (filesToUpload == null || filesToUpload.length < 1) {
+			$('#uploadErrors').text('ERROR: No media files selected for upload. Use the "Choose Files" button to select them.');
+			return
+		}
+
+		const attName = 'media';
+		for (let i = 0; i < filesToUpload.length; i++) {
+			const uploadFile = filesToUpload[i];
+			const uploadFileId = await getUuid();
+
+			const newDoc = selectedSchema.createObject({});
+			newDoc[templateDoc.nunaliit_schema] = templateDoc[templateDoc.nunaliit_schema];
+
+			newDoc.nunaliit_attachments.files[attName] = {
+				attachmentName: attName
+				, status: "waiting for upload"
+				, data: {}
+				, uploadId: uploadFileId
+			};
+
+			let docInfo = {
+				id: '',
+				uploadId: uploadFileId,
+				filename: uploadFile.name,
+				state: 'creating'
+			};
+			createdDocs.push(docInfo);
+			refreshStateTable(createdDocs);
+
+			try {
+				const createdDoc = await createDocument(newDoc);
+				docInfo.state = 'uploading media';
+				docInfo.id = createdDoc._id;
+				refreshStateTable(createdDocs);
+
+				await uploadMediaFile(uploadFile, i, uploadFileId);
+				docInfo.state = 'done';
+			} catch (err) {
+				$('#uploadErrors').text('Error creating or uploading documents, see table for details.');
+				docInfo.state = `ERROR: ${err}`;
+			}
+			refreshStateTable(createdDocs);
+		}
+	}
 
 	function main(opts_) {
 		var opts = $n2.extend({
@@ -420,18 +237,10 @@
 		var config = opts.config;
 
 		$n2.log('main', config);
-		atlasDb = config.atlasDb;
-		atlasDesign = config.atlasDesign;
 		uploadService = config.directory.uploadService;
-		searchServer = config.directory.searchService;
-		requests = config.directory.requestService;
-		contributions = config.contributions;
-		couchEditor = config.couchEditor;
 		schemaEditorService = config.directory.schemaEditorService;
-		showService = config.directory.showService;
 		schemaRepository = config.directory.schemaRepository;
 		dispatcher = config.directory.dispatchService;
-		createDocProcess = config.directory.createDocProcess;
 		dialogService = config.directory.dialogService;
 		documentSource = config.documentSource;
 
@@ -440,33 +249,11 @@
 		$('#selectSchemaButton').click(addDocument);
 		$('#uploadFiles').change(function (event) {
 			filesToUpload = event.target.files;
-			for (let i = 0; i < filesToUpload.length; i++) {
-				console.log(filesToUpload[i].name);
-			}
-
 		});
 		$('#uploadForm').submit(function (event) {
 			uploadFiles(event);
-
 			event.preventDefault();
 		});
-
-		// Editor
-		couchEditor.options.enableAddFile = true;
-		// couchEditor.options.onCloseFn = function(){
-		// 	$('#schemaValues').empty();
-		// };
-		schemaRepository.getSchema({
-			name: 'object'
-			, onSuccess: function (schema) {
-				defaultSchema = schema;
-				couchEditor.options.schema = defaultSchema;
-			}
-			, onError: function (err) {
-				$n2.log('Unable to load schema for editor', err);
-			}
-		});
-
 		config.start();
 	};
 
