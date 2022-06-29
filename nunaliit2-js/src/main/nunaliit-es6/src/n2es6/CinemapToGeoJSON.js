b85663fcc49b0e7edcba3066be5c77682db9c0c5
alexgao1 <alexgao1@users.noreply.github.com>
2022-06-29 10:58:43 -0400

    Generate rest of the properties

--- nunaliit2-js/src/main/nunaliit-es6/src/n2es6/CinemapToGeoJSON.js
+++ nunaliit2-js/src/main/nunaliit-es6/src/n2es6/CinemapToGeoJSON.js
@@ -69,25 +69,56 @@ class CinemapToGeoJSON {
     static generateFeatures(baseExport, places) {
         if (!baseExport) return;
         if (!places) return;
-        const timeLinks = baseExport.timeLinks;
+        const { timeLinks, ...restOfExport } = baseExport;
         const features = [];
         timeLinks.forEach((line, index) => {
-            const tags = line.tags.map(tag => {
-                delete tag.chipText;
-                return tag;
-            });
-            // decide here if generating multipoint or points
-            // need to .trim().toLowerCase() to match in places
-            const geometry = {};
             const feature = {
                 type: "Feature"
                 , id: index
             };
+            const properties = {
+                timeLink: index
+                , starttime: line.starttime
+                , endtime: line.endtime
+                , tags: line.tags.map(tag => {
+                    return {
+                        value: tag.value
+                        , type: tag.type
+                    }
+                })
+            };
+            feature.properties = properties;
+            const placeTags = line.tags.filter(tag => {
+                return (tag.type && tag.type === "place")
+            });
+            const placeCount = placeTags.length;
+            if (placeCount > 0) {
+                const geometry = {
+                    coordinates: []
+                };
+                let geomType = "UNKNOWN_GEOM_TYPE";
+                if (placeCount === 1) {
+                    geomType = "Point";
+                }
+                else if (placeCount > 1) {
+                    geomType = "MultiPoint";
+                }
+                geometry.type = geomType;
+                placeTags.forEach(place => {
+                    geometry.coordinates.push(
+                        places[place.value.trim().toLowerCase()].nunaliit_geom.bbox.slice(0,2).reverse()
+                    );
+                });
+                feature.geometry = geometry;
+            }            
             features.push(feature);
         });
-        delete baseExport.timeLinks;
-        baseExport.features = features;
-        return baseExport;
+        // reference error here, don't modify baseExport
+        const exportWithFeatures = {
+            ...restOfExport,
+            features
+        }
+        return exportWithFeatures;
     }
 
     // https://stackoverflow.com/a/65939108
