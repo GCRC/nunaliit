581751b70996d2caed3006150a810f9245bbf00f
Benjamin Brown <86317244+bnbrown2@users.noreply.github.com>
2023-01-17 09:40:04 -0900

    Scale Line (#1108)
    Add scaleline config option for OL6 impl.

--- nunaliit2-js/src/main/nunaliit-es6/src/n2es6/n2mapModule/N2MapCanvas.js
+++ nunaliit2-js/src/main/nunaliit-es6/src/n2es6/n2mapModule/N2MapCanvas.js
@@ -47,6 +47,7 @@ import Swipe from 'ol-ext/control/Swipe';
 import { defaults as Defaults } from 'ol/control';
 
 import N2MapSpy from './N2MapSpy';
+import N2MapScale from './N2MapScale';
 
 const _loc = function (str, args) { return $n2.loc(str, 'nunaliit2', args); };
 const DH = 'n2.canvasMap';
@@ -762,6 +763,15 @@ class N2MapCanvas {
 			};
 			spyCtrl = new N2MapSpy(data); 
 			customMap.addControl(spyCtrl);
+		}
+
+		let scaleCtrl;
+		if (this.options.scaleLine === true || this.options.scaleLine === "true"){
+			const data = {
+				unit : this.options.scaleUnit
+			};
+			scaleCtrl = new N2MapScale(data);
+			customMap.addControl(scaleCtrl);
 		}
 
 		this.overlayInfos.forEach( (info, idx) => {
