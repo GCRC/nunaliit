65f721a2ec4ea7e529a8fb0f0b221c3b22a00cb8
Jagdish Kunwar <jkunawr@lunageo.com>
2023-01-25 11:37:09 -0500

    check media stream if the file has audio//video streams

---
+++ nunaliit2-multimedia/src/main/java/ca/carleton/gcrc/olkit/multimedia/ffmpeg/FFprobeProcessor.java
@@ -0,0 +1,54 @@
+package ca.carleton.gcrc.olkit.multimedia.ffmpeg;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import java.io.BufferedReader;
+import org.json.JSONObject;
+import org.json.JSONArray;
+import java.util.stream.Collectors;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class FFprobeProcessor {
+
+    private static final Logger log = LoggerFactory.getLogger(FFprobeProcessor.class);
+
+    static public String ffprobeCommand = "ffprobe -v quiet -print_format json -show_streams %s";
+
+    private FFprobeProcessor() {
+    }
+
+    public static List<FileStream> getFileStreams(File file) {
+        String cmd = String.format(ffprobeCommand, file.getPath());
+        Process process = null;
+        try {
+            process = Runtime.getRuntime().exec(cmd);
+            BufferedReader br = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            String output = br.lines().collect(Collectors.joining("\n"));
+            JSONObject json = new JSONObject(output);
+            JSONArray streams = json.getJSONArray("streams");
+
+            List<FileStream> fileStreams = new ArrayList<FileStream>();
+            for (int i = 0; i < streams.length(); i++) {
+                FileStream fileStream = new FileStream();
+                fileStream.setCodecType(streams.getJSONObject(i).getString("codec_type"));
+                fileStreams.add(fileStream);
+            }
+            return fileStreams;
+        } catch (IOException e) {
+            log.warn("Problem getting file streams: {}", e.getMessage());
+            return null;
+        } finally {
+            if (process != null) {
+                process.destroy();
+            }
+        }
+    }
+
+}
---
+++ nunaliit2-multimedia/src/main/java/ca/carleton/gcrc/olkit/multimedia/ffmpeg/FileStream.java
@@ -0,0 +1,14 @@
+package ca.carleton.gcrc.olkit.multimedia.ffmpeg;
+
+public class FileStream {
+
+    public String codecType;
+
+    public void setCodecType(String codecType) {
+        this.codecType = codecType;
+    }
+
+    public String getCodecType() {
+        return this.codecType;
+    }
+}
