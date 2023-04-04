39c22d319388d808882717502217bac232816d20
kunwarj <121902036+kunwarj@users.noreply.github.com>
2023-04-04 13:43:17 -0400

    Email administrators upon user registration (#1144)
    * add options to send email notification to admin whenever a new user is created
    
    * fit code reviews
    
    * fix indentation
    
    ---------
    
    Co-authored-by: Jagdish Kunwar <jkunwar@luageo.com>

---
+++ nunaliit2-couch-user/src/main/java/ca/carleton/gcrc/couch/user/mail/UserRegistrationGenerator.java
@@ -0,0 +1,40 @@
+package ca.carleton.gcrc.couch.user.mail;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.Map;
+
+import ca.carleton.gcrc.mail.MailMessage;
+import ca.carleton.gcrc.mail.messageGenerator.MailMessageGenerator;
+
+class UserRegistrationGenerator implements MailMessageGenerator {
+
+	public UserRegistrationGenerator(){
+
+	}
+
+	@Override
+	public void generateMessage(
+			MailMessage message,
+			Map<String,String> parameters
+			) throws Exception {
+
+		String link = parameters.get("link");
+		String userEmail = parameters.get("userEmail");
+
+		// Subject
+		message.setSubject("Nunaliit User Registration");
+
+		// Create HTML body part
+		StringWriter sw = new StringWriter();
+		PrintWriter pw = new PrintWriter(sw);
+		pw.println("<html><head><title>Nunaliit User Registration</title></head><body><h1>Nunaliit User Registration</h1>");
+		pw.println("<p>A new user <b>" +userEmail +"</b> has registered with the atlas. ");
+		if( null !=  link) {
+			pw.println("<p><a href=\""+link+"\">"+link+"</a></p>");
+		}
+		pw.println("</body></html>");
+		pw.flush();
+		message.setHtmlContent(sw.toString());
+	}
+}
