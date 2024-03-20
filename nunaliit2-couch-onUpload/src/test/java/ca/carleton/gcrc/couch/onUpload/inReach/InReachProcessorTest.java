38d800e35ae0a29906805395a5b6990d66a2778a
alexgao1 <alexgao1@users.noreply.github.com>
2024-03-20 10:31:00 -0400

    formatting

--- nunaliit2-couch-onUpload/src/test/java/ca/carleton/gcrc/couch/onUpload/inReach/InReachProcessorTest.java
+++ nunaliit2-couch-onUpload/src/test/java/ca/carleton/gcrc/couch/onUpload/inReach/InReachProcessorTest.java
@@ -18,54 +18,54 @@ public class InReachProcessorTest extends TestCase {
 	public void testPerformSubmission() throws Exception {
 		File settingsFile = TestSupport.findResourceFile("inreach_forms.xml");
 		File docFile = TestSupport.findResourceFile("inreach_doc.json");
-		
+
 		InReachSettingsFromXmlFile settings = new InReachSettingsFromXmlFile(settingsFile);
 		settings.load();
 		InReachConfiguration.setInReachSettings(settings);
-		
+
 		InReachProcessorImpl processor = new InReachProcessorImpl();
-		
+
 		JSONObject jsonDoc = TextFileUtils.readJsonObjectFile(docFile);
-		
+
 		MockFileConversionContext2 conversionContext = new MockFileConversionContext2(jsonDoc);
-		
+
 		processor.performSubmission(conversionContext);
-		
+
 		JSONObject savedDocJson = conversionContext.getSavedDocument();
-		if( null == savedDocJson ){
+		if (null == savedDocJson) {
 			fail("Document not saved");
 		}
 		MockFileConversionContext2 savedContext = new MockFileConversionContext2(savedDocJson);
 		DocumentDescriptor savedDoc = savedContext.getDocument();
-		
+
 		// Test geometry
 		GeometryDescriptor savedGeomDesc = savedDoc.getGeometryDescription();
 		Geometry savedGeom = savedGeomDesc.getGeometry();
-		
-		if( savedGeom instanceof Point ){
-			Point point = (Point)savedGeom;
-			if( point.getX() < -75.65 
-			 || point.getX() > -75.55 ){
-				fail("Point Longitude should be -75.6: "+point.getX());
+
+		if (savedGeom instanceof Point) {
+			Point point = (Point) savedGeom;
+			if (point.getX() < -75.65
+					|| point.getX() > -75.55) {
+				fail("Point Longitude should be -75.6: " + point.getX());
 			}
-			if( point.getY() < 45.25 
-			 || point.getY() > 45.35 ){
-				fail("Point Latitude should be 45.3: "+point.getY());
+			if (point.getY() < 45.25
+					|| point.getY() > 45.35) {
+				fail("Point Latitude should be 45.3: " + point.getY());
 			}
 		} else {
 			fail("Geometry should be a Point");
 		}
-		
+
 		// Test schema name
 		String schemaName = savedDoc.getSchemaName();
-		if( false == "inReach_Conditions".equals(schemaName) ){
-			fail("Unexpected schema name: "+schemaName);
+		if (false == "inReach_Conditions".equals(schemaName)) {
+			fail("Unexpected schema name: " + schemaName);
 		}
-		
+
 		// Check data
 		JSONObject data = savedDocJson.getJSONObject("inReach_Conditions");
 		String condition = data.optString("Condition", null);
-		if( false == "high water".equals(condition) ){
+		if (false == "high water".equals(condition)) {
 			fail("Unexpected data");
 		}
 	}
@@ -73,54 +73,54 @@ public class InReachProcessorTest extends TestCase {
 	public void testPerformGarminExploreSubmission() throws Exception {
 		File settingsFile = TestSupport.findResourceFile("inreach_forms.xml");
 		File docFile = TestSupport.findResourceFile("inreach_garminexplore_doc.json");
-		
+
 		InReachSettingsFromXmlFile settings = new InReachSettingsFromXmlFile(settingsFile);
 		settings.load();
 		InReachConfiguration.setInReachSettings(settings);
-		
+
 		InReachProcessorImpl processor = new InReachProcessorImpl();
-		
+
 		JSONObject jsonDoc = TextFileUtils.readJsonObjectFile(docFile);
-		
+
 		MockFileConversionContext2 conversionContext = new MockFileConversionContext2(jsonDoc);
-		
+
 		processor.performSubmission(conversionContext);
-		
+
 		JSONObject savedDocJson = conversionContext.getSavedDocument();
-		if( null == savedDocJson ){
+		if (null == savedDocJson) {
 			fail("Document not saved");
 		}
 		MockFileConversionContext2 savedContext = new MockFileConversionContext2(savedDocJson);
 		DocumentDescriptor savedDoc = savedContext.getDocument();
-		
+
 		// Test geometry
 		GeometryDescriptor savedGeomDesc = savedDoc.getGeometryDescription();
 		Geometry savedGeom = savedGeomDesc.getGeometry();
-		
-		if( savedGeom instanceof Point ){
-			Point point = (Point)savedGeom;
-			if( point.getX() < -75.75
-			 || point.getX() > -75.73 ){
-				fail("Point Longitude should be -75.74515342712403: "+point.getX());
+
+		if (savedGeom instanceof Point) {
+			Point point = (Point) savedGeom;
+			if (point.getX() < -75.75
+					|| point.getX() > -75.73) {
+				fail("Point Longitude should be -75.74515342712403: " + point.getX());
 			}
-			if( point.getY() < 45.38 
-			 || point.getY() > 45.4 ){
-				fail("Point Latitude should be 45.3940486907959: "+point.getY());
+			if (point.getY() < 45.38
+					|| point.getY() > 45.4) {
+				fail("Point Latitude should be 45.3940486907959: " + point.getY());
 			}
 		} else {
 			fail("Geometry should be a Point");
 		}
-		
+
 		// Test schema name
 		String schemaName = savedDoc.getSchemaName();
-		if( false == "inReach_Wildlife".equals(schemaName) ){
-			fail("Unexpected schema name: "+schemaName);
+		if (false == "inReach_Wildlife".equals(schemaName)) {
+			fail("Unexpected schema name: " + schemaName);
 		}
-		
+
 		// Check data
 		JSONObject data = savedDocJson.getJSONObject("inReach_Wildlife");
 		String condition = data.optString("What", null);
-		if( false == "Caribou".equals(condition) ){
+		if (false == "Caribou".equals(condition)) {
 			fail("Unexpected data");
 		}
 	}
@@ -128,43 +128,43 @@ public class InReachProcessorTest extends TestCase {
 	public void testMissingField() throws Exception {
 		File settingsFile = TestSupport.findResourceFile("inreach_forms.xml");
 		File docFile = TestSupport.findResourceFile("inreach_doc_missing.json");
-		
+
 		InReachSettingsFromXmlFile settings = new InReachSettingsFromXmlFile(settingsFile);
 		settings.load();
 		InReachConfiguration.setInReachSettings(settings);
-		
+
 		InReachProcessorImpl processor = new InReachProcessorImpl();
-		
+
 		JSONObject jsonDoc = TextFileUtils.readJsonObjectFile(docFile);
-		
+
 		MockFileConversionContext2 conversionContext = new MockFileConversionContext2(jsonDoc);
-		
+
 		processor.performSubmission(conversionContext);
-		
+
 		JSONObject savedDocJson = conversionContext.getSavedDocument();
-		if( null == savedDocJson ){
+		if (null == savedDocJson) {
 			fail("Document not saved");
 		}
 		MockFileConversionContext2 savedContext = new MockFileConversionContext2(savedDocJson);
 		DocumentDescriptor savedDoc = savedContext.getDocument();
-		
+
 		// Test schema name
 		String schemaName = savedDoc.getSchemaName();
-		if( false == "inReach_Conditions".equals(schemaName) ){
-			fail("Unexpected schema name: "+schemaName);
+		if (false == "inReach_Conditions".equals(schemaName)) {
+			fail("Unexpected schema name: " + schemaName);
 		}
-		
+
 		// Check data
 		JSONObject data = savedDocJson.getJSONObject("inReach_Conditions");
 		{
 			String condition = data.optString("Condition", null);
-			if( false == "other (put in notes)".equals(condition) ){
+			if (false == "other (put in notes)".equals(condition)) {
 				fail("Unexpected data for 'Condition'");
 			}
 		}
 		{
-			String probs = data.optString("Causing problems?",null);
-			if( null != probs ){
+			String probs = data.optString("Causing problems?", null);
+			if (null != probs) {
 				fail("Unexpected data for 'Causing problems?'");
 			}
 		}
@@ -173,32 +173,32 @@ public class InReachProcessorTest extends TestCase {
 	public void testGpsTimestamp() throws Exception {
 		File settingsFile = TestSupport.findResourceFile("inreach_forms.xml");
 		File docFile = TestSupport.findResourceFile("inreach_doc_missing.json");
-		
+
 		InReachSettingsFromXmlFile settings = new InReachSettingsFromXmlFile(settingsFile);
 		settings.load();
 		InReachConfiguration.setInReachSettings(settings);
-		
+
 		InReachProcessorImpl processor = new InReachProcessorImpl();
-		
+
 		JSONObject jsonDoc = TextFileUtils.readJsonObjectFile(docFile);
-		
+
 		MockFileConversionContext2 conversionContext = new MockFileConversionContext2(jsonDoc);
-		
+
 		processor.performSubmission(conversionContext);
-		
+
 		JSONObject savedDocJson = conversionContext.getSavedDocument();
-		if( null == savedDocJson ){
+		if (null == savedDocJson) {
 			fail("Document not saved");
 		}
 
 		// Look for time structure
 		JSONObject jsonItem = savedDocJson.getJSONObject("inReach");
 		JSONObject jsonTimestamp = jsonItem.optJSONObject("nunaliit_gps_datetime");
-		if( null == jsonTimestamp ){
+		if (null == jsonTimestamp) {
 			fail("Can not find nunaliit_gps_datetime!");
 		} else {
 			String dateType = jsonTimestamp.optString("nunaliit_type", null);
-			if( false == "date".equals(dateType) ) {
+			if (false == "date".equals(dateType)) {
 				fail("Unexpected time structure");
 			}
 		}
