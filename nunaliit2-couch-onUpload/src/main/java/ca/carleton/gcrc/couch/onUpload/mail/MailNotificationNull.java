6091b0f48b81083f0e3a14edf60b708ba87f3788
alexgao1 <alexgao1@users.noreply.github.com>
2024-04-17 14:11:42 -0400

    need impl for null main notification

--- nunaliit2-couch-onUpload/src/main/java/ca/carleton/gcrc/couch/onUpload/mail/MailNotificationNull.java
+++ nunaliit2-couch-onUpload/src/main/java/ca/carleton/gcrc/couch/onUpload/mail/MailNotificationNull.java
@@ -1,12 +1,14 @@
 package ca.carleton.gcrc.couch.onUpload.mail;
 
+import org.json.JSONObject;
+
+import ca.carleton.gcrc.couch.user.UserDocument;
+
 public class MailNotificationNull implements MailNotification {
 
 	@Override
 	public void uploadNotification(
-			String docId
-			,String attachmentName
-			) throws Exception {
+			String docId, String attachmentName) throws Exception {
 		// Do nothing
 	}
 
@@ -15,4 +17,10 @@ public class MailNotificationNull implements MailNotification {
 		// Do nothing
 	}
 
+	@Override
+	public void sendDocumentCreatedNotification(
+			JSONObject doc,
+			UserDocument currentUser) throws Exception {
+				// Do nothing
+	}
 }
