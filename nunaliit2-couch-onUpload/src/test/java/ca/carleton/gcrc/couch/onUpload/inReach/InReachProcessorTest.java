b17f915ab17ac0cae866f9a44d5aed6d63af544d
alexgao1 <alexgao1@users.noreply.github.com>
2024-05-21 20:54:46 +0000

    Feature: Garmin Explore inReach messages (#1221)
    * tmp
    
    * format file
    
    * add checking for type of provider message, move handling to functions
    
    * Add Garmin Explore message codes, WIP copy data from GarminExplore format into Nunaliit format
    
    * remove unnecessary code
    
    * more WIP Garmin format conversion
    
    * Generate geom and put generic nunaliit metadata
    
    * change form extraction param
    
    * typos
    
    * checks
    
    * formatting
    
    * Server work view checks if new inReach message type
    
    * forget what string comparison in java is like
    
    * set nunaliit_schema to prevent looping work
    
    * .
    
    * fix unix timestamp to iso timestamp, reset schemaName when processing loops, add another message code type
    
    * Need to print 9 digits for nanoseconds, need to write POINT instead of MULTIPOINT for JS code
    
    * Remove unused imports, add test function for Garmin Explore type message, test file
    
    * formatting
    
    * Fix test for single Garmin Explore case
    
    * add multiple event garmin explore type message
    
    * add more test code for emergencystates, messagetypes
    
    * add more messagecode types that appear to be in use
    
    * Set nunaliit_inreach object on original GarminExplore messages, get UUIDs for new documents, do not make geom for 0,0 messages, set source message ID in created documents
    
    * Emit work for new kind of inReach properties
    
    * comment
    
    * Get client for UUIDs for new documents, fix tests to  handle changes
    
    * Get latest document to save eventDocIds in original message
    
    * formatting
    
    * remove any non-email address recipients
    
    * enum to set status on event process rejection, get form recipients to redact entire messages if not found
    
    * log error do not throw exception

--- nunaliit2-couch-onUpload/src/test/java/ca/carleton/gcrc/couch/onUpload/inReach/InReachProcessorTest.java
+++ nunaliit2-couch-onUpload/src/test/java/ca/carleton/gcrc/couch/onUpload/inReach/InReachProcessorTest.java
@@ -1,9 +1,12 @@
 package ca.carleton.gcrc.couch.onUpload.inReach;
 
 import java.io.File;
+import java.util.List;
 
 import org.json.JSONObject;
 
+import ca.carleton.gcrc.couch.client.CouchClient;
+import ca.carleton.gcrc.couch.onUpload.MockCouchClient;
 import ca.carleton.gcrc.couch.onUpload.MockFileConversionContext2;
 import ca.carleton.gcrc.couch.onUpload.TestSupport;
 import ca.carleton.gcrc.couch.onUpload.conversion.DocumentDescriptor;
@@ -15,101 +18,293 @@ import junit.framework.TestCase;
 
 public class InReachProcessorTest extends TestCase {
 
+	private CouchClient testCouchClient;
+
 	public void testPerformSubmission() throws Exception {
 		File settingsFile = TestSupport.findResourceFile("inreach_forms.xml");
 		File docFile = TestSupport.findResourceFile("inreach_doc.json");
-		
+
 		InReachSettingsFromXmlFile settings = new InReachSettingsFromXmlFile(settingsFile);
 		settings.load();
 		InReachConfiguration.setInReachSettings(settings);
-		
-		InReachProcessorImpl processor = new InReachProcessorImpl();
-		
+
+		InReachProcessorImpl processor = new InReachProcessorImpl(null);
+
 		JSONObject jsonDoc = TextFileUtils.readJsonObjectFile(docFile);
-		
+
 		MockFileConversionContext2 conversionContext = new MockFileConversionContext2(jsonDoc);
-		
+
 		processor.performSubmission(conversionContext);
-		
+
 		JSONObject savedDocJson = conversionContext.getSavedDocument();
-		if( null == savedDocJson ){
+		if (null == savedDocJson) {
 			fail("Document not saved");
 		}
 		MockFileConversionContext2 savedContext = new MockFileConversionContext2(savedDocJson);
 		DocumentDescriptor savedDoc = savedContext.getDocument();
-		
+
 		// Test geometry
 		GeometryDescriptor savedGeomDesc = savedDoc.getGeometryDescription();
 		Geometry savedGeom = savedGeomDesc.getGeometry();
-		
-		if( savedGeom instanceof Point ){
-			Point point = (Point)savedGeom;
-			if( point.getX() < -75.65 
-			 || point.getX() > -75.55 ){
-				fail("Point Longitude should be -75.6: "+point.getX());
+
+		if (savedGeom instanceof Point) {
+			Point point = (Point) savedGeom;
+			if (point.getX() < -75.65
+					|| point.getX() > -75.55) {
+				fail("Point Longitude should be -75.6: " + point.getX());
 			}
-			if( point.getY() < 45.25 
-			 || point.getY() > 45.35 ){
-				fail("Point Latitude should be 45.3: "+point.getY());
+			if (point.getY() < 45.25
+					|| point.getY() > 45.35) {
+				fail("Point Latitude should be 45.3: " + point.getY());
 			}
 		} else {
 			fail("Geometry should be a Point");
 		}
-		
+
 		// Test schema name
 		String schemaName = savedDoc.getSchemaName();
-		if( false == "inReach_Conditions".equals(schemaName) ){
-			fail("Unexpected schema name: "+schemaName);
+		if (false == "inReach_Conditions".equals(schemaName)) {
+			fail("Unexpected schema name: " + schemaName);
 		}
-		
+
 		// Check data
 		JSONObject data = savedDocJson.getJSONObject("inReach_Conditions");
 		String condition = data.optString("Condition", null);
-		if( false == "high water".equals(condition) ){
+		if (false == "high water".equals(condition)) {
 			fail("Unexpected data");
+		}
+	}
+
+	public void testPerformGarminExploreSubmission() throws Exception {
+		try {
+			testCouchClient = new MockCouchClient();
+		} catch (Exception e) {
+			fail("Could not init testing Couch client:" + e);
+		}
+		File settingsFile = TestSupport.findResourceFile("inreach_forms.xml");
+		File docFile = TestSupport.findResourceFile("inreach_garminexplore_doc.json");
+
+		InReachSettingsFromXmlFile settings = new InReachSettingsFromXmlFile(settingsFile);
+		settings.load();
+		InReachConfiguration.setInReachSettings(settings);
+
+		InReachProcessorImpl processor = new InReachProcessorImpl(testCouchClient);
+		JSONObject jsonDoc = TextFileUtils.readJsonObjectFile(docFile);
+		MockFileConversionContext2 conversionContext = new MockFileConversionContext2(jsonDoc);
+		processor.performSubmission(conversionContext);
+
+		JSONObject savedDocJson = conversionContext.getSavedDocument();
+		if (null == savedDocJson) {
+			fail("Document not saved");
+		}
+
+		List<JSONObject> resDocs = conversionContext.getCreatedDocuments();
+		JSONObject singleCreatedDocument = null;
+		if (resDocs.size() == 1) {
+			singleCreatedDocument = resDocs.get(0);
+		} else {
+			fail("Exactly one new document should be created with inreach_garminexplore_doc.json");
+		}
+		MockFileConversionContext2 savedContext = new MockFileConversionContext2(singleCreatedDocument);
+		DocumentDescriptor savedDoc = savedContext.getDocument();
+		GeometryDescriptor savedGeomDesc = savedDoc.getGeometryDescription();
+		Geometry savedGeom = savedGeomDesc.getGeometry();
+
+		if (savedGeom instanceof Point) {
+			Point point = (Point) savedGeom;
+			if (point.getX() < -75.75
+					|| point.getX() > -75.73) {
+				fail("Point Longitude should be -75.74515342712403: " + point.getX());
+			}
+			if (point.getY() < 45.38
+					|| point.getY() > 45.4) {
+				fail("Point Latitude should be 45.3940486907959: " + point.getY());
+			}
+		} else {
+			fail("Geometry should be a Point");
+		}
+
+		String schemaName = savedDoc.getSchemaName();
+		if (false == "inReach_Wildlife".equals(schemaName)) {
+			fail("Unexpected schema name: " + schemaName);
+		}
+
+		JSONObject data = singleCreatedDocument.getJSONObject("inReach_Wildlife");
+		String condition = data.optString("What", null);
+		if (false == "Caribou".equals(condition)) {
+			fail("Unexpected data");
+		}
+	}
+
+	public void testPerformGarminExploreMultipleEventSubmission() throws Exception {
+		try {
+			testCouchClient = new MockCouchClient();
+		} catch (Exception e) {
+			fail("Could not init testing Couch client:" + e);
+		}
+		File settingsFile = TestSupport.findResourceFile("inreach_forms.xml");
+		File docFile = TestSupport.findResourceFile("inreach_garminexplore_multi_doc.json");
+
+		InReachSettingsFromXmlFile settings = new InReachSettingsFromXmlFile(settingsFile);
+		settings.load();
+		InReachConfiguration.setInReachSettings(settings);
+
+		InReachProcessorImpl processor = new InReachProcessorImpl(testCouchClient);
+		JSONObject jsonDoc = TextFileUtils.readJsonObjectFile(docFile);
+		MockFileConversionContext2 conversionContext = new MockFileConversionContext2(jsonDoc);
+		processor.performSubmission(conversionContext);
+
+		JSONObject savedDocJson = conversionContext.getSavedDocument();
+		if (null == savedDocJson) {
+			fail("Document not saved");
+		}
+
+		List<JSONObject> resDocs = conversionContext.getCreatedDocuments();
+		if (resDocs.size() != 9) {
+			fail("Exactly nine new documents should be created with inreach_garminexplore_multi_doc.json (last one fails to create document, phone number messages are removed)");
+		}
+
+		for (int i = 0; i < resDocs.size(); i++) {
+			JSONObject createdDocument = resDocs.get(i);
+			MockFileConversionContext2 savedContext = new MockFileConversionContext2(createdDocument);
+			DocumentDescriptor savedDoc = savedContext.getDocument();
+			String schemaName = savedDoc.getSchemaName();
+			JSONObject jsonItem = createdDocument.getJSONObject("inReach");
+			JSONObject jsonTimestamp = jsonItem.optJSONObject("nunaliit_gps_datetime");
+			String messageType = jsonItem.getString("MessageType");
+			Integer emergencyState = jsonItem.getInt("EmergencyState");
+
+			if (i == 0) {
+				if (false == "inReach_Wildlife".equals(schemaName)) {
+					fail("Unexpected schema name: " + schemaName);
+				}
+				if (!messageType.equals("FreeTextMessage")) {
+					fail("Wrong MessageType from created document: " + messageType);
+				}
+				if (emergencyState != -1) {
+					fail("EmergencyState expected to be -1");
+				}
+			} else if (i == 1) {
+				if (false == "inReach_Place".equals(schemaName)) {
+					fail("Unexpected schema name: " + schemaName);
+				}
+				if (!messageType.equals("FreeTextMessage")) {
+					fail("Wrong MessageType from created document: " + messageType);
+				}
+				if (emergencyState != -1) {
+					fail("EmergencyState expected to be -1");
+				}
+			} else if (i == 2) {
+				if (false == "inReach_Issues".equals(schemaName)) {
+					fail("Unexpected schema name: " + schemaName);
+				}
+				if (!messageType.equals("FreeTextMessage")) {
+					fail("Wrong MessageType from created document: " + messageType);
+				}
+				if (emergencyState != -1) {
+					fail("EmergencyState expected to be -1");
+				}
+			} else if (i == 3) {
+				if (false == "inReach".equals(schemaName)) {
+					fail("Unexpected schema name: " + schemaName);
+				}
+				if (!messageType.equals("MailCheck")) {
+					fail("Wrong MessageType from created document: " + messageType);
+				}
+				if (emergencyState != -1) {
+					fail("EmergencyState expected to be -1");
+				}
+			} else if (i == 4) {
+				if (!messageType.equals("NunaliitUnhandledGarminExploreMessageCode-5")) {
+					fail("Expected unhandled message code string for unimplemented/unknown message codes");
+				}
+				if (emergencyState != -1) {
+					fail("EmergencyState expected to be -1");
+				}
+			} else if (i == 5) {
+				if (!messageType.equals("DeclareSOS")) {
+					fail("Wrong MessageType from created document: " + messageType);
+				}
+				if (emergencyState == -1) {
+					fail("EmergencyState should be set");
+				}
+			} else if (i == 6) {
+				if (!messageType.equals("ConfirmSOS")) {
+					fail("Wrong MessageType from created document: " + messageType);
+				}
+				if (emergencyState == -1) {
+					fail("EmergencyState should be set");
+				}
+			} else if (i == 7) {
+				if (!messageType.equals("CancelSOS")) {
+					fail("Wrong MessageType from created document: " + messageType);
+				}
+				if (emergencyState == -1) {
+					fail("EmergencyState should be set");
+				}
+			} else if (i == 8) {
+				if (!messageType.equals("NunaliitUnhandledGarminExploreMessageCode-100")) {
+					fail("Wrong MessageType from created document: " + messageType);
+				}
+				try {
+					createdDocument.getJSONObject("nunaliit_geom");
+					fail("No geom should be created when latitude and longitude are 0");
+				} catch (Exception e) {
+				}
+
+			}
+
+			if (null == jsonTimestamp) {
+				fail("Can not find nunaliit_gps_datetime!");
+			} else {
+				String dateType = jsonTimestamp.optString("nunaliit_type", null);
+				if (false == "date".equals(dateType)) {
+					fail("Unexpected time structure");
+				}
+			}
 		}
 	}
 
 	public void testMissingField() throws Exception {
 		File settingsFile = TestSupport.findResourceFile("inreach_forms.xml");
 		File docFile = TestSupport.findResourceFile("inreach_doc_missing.json");
-		
+
 		InReachSettingsFromXmlFile settings = new InReachSettingsFromXmlFile(settingsFile);
 		settings.load();
 		InReachConfiguration.setInReachSettings(settings);
-		
-		InReachProcessorImpl processor = new InReachProcessorImpl();
-		
+
+		InReachProcessorImpl processor = new InReachProcessorImpl(null);
+
 		JSONObject jsonDoc = TextFileUtils.readJsonObjectFile(docFile);
-		
+
 		MockFileConversionContext2 conversionContext = new MockFileConversionContext2(jsonDoc);
-		
+
 		processor.performSubmission(conversionContext);
-		
+
 		JSONObject savedDocJson = conversionContext.getSavedDocument();
-		if( null == savedDocJson ){
+		if (null == savedDocJson) {
 			fail("Document not saved");
 		}
 		MockFileConversionContext2 savedContext = new MockFileConversionContext2(savedDocJson);
 		DocumentDescriptor savedDoc = savedContext.getDocument();
-		
+
 		// Test schema name
 		String schemaName = savedDoc.getSchemaName();
-		if( false == "inReach_Conditions".equals(schemaName) ){
-			fail("Unexpected schema name: "+schemaName);
+		if (false == "inReach_Conditions".equals(schemaName)) {
+			fail("Unexpected schema name: " + schemaName);
 		}
-		
+
 		// Check data
 		JSONObject data = savedDocJson.getJSONObject("inReach_Conditions");
 		{
 			String condition = data.optString("Condition", null);
-			if( false == "other (put in notes)".equals(condition) ){
+			if (false == "other (put in notes)".equals(condition)) {
 				fail("Unexpected data for 'Condition'");
 			}
 		}
 		{
-			String probs = data.optString("Causing problems?",null);
-			if( null != probs ){
+			String probs = data.optString("Causing problems?", null);
+			if (null != probs) {
 				fail("Unexpected data for 'Causing problems?'");
 			}
 		}
@@ -118,32 +313,32 @@ public class InReachProcessorTest extends TestCase {
 	public void testGpsTimestamp() throws Exception {
 		File settingsFile = TestSupport.findResourceFile("inreach_forms.xml");
 		File docFile = TestSupport.findResourceFile("inreach_doc_missing.json");
-		
+
 		InReachSettingsFromXmlFile settings = new InReachSettingsFromXmlFile(settingsFile);
 		settings.load();
 		InReachConfiguration.setInReachSettings(settings);
-		
-		InReachProcessorImpl processor = new InReachProcessorImpl();
-		
+
+		InReachProcessorImpl processor = new InReachProcessorImpl(null);
+
 		JSONObject jsonDoc = TextFileUtils.readJsonObjectFile(docFile);
-		
+
 		MockFileConversionContext2 conversionContext = new MockFileConversionContext2(jsonDoc);
-		
+
 		processor.performSubmission(conversionContext);
-		
+
 		JSONObject savedDocJson = conversionContext.getSavedDocument();
-		if( null == savedDocJson ){
+		if (null == savedDocJson) {
 			fail("Document not saved");
 		}
 
 		// Look for time structure
 		JSONObject jsonItem = savedDocJson.getJSONObject("inReach");
 		JSONObject jsonTimestamp = jsonItem.optJSONObject("nunaliit_gps_datetime");
-		if( null == jsonTimestamp ){
+		if (null == jsonTimestamp) {
 			fail("Can not find nunaliit_gps_datetime!");
 		} else {
 			String dateType = jsonTimestamp.optString("nunaliit_type", null);
-			if( false == "date".equals(dateType) ) {
+			if (false == "date".equals(dateType)) {
 				fail("Unexpected time structure");
 			}
 		}
